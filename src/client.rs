f6544c0de087e78926ccc1cd21fa3109110e50c9
NelsonMQ <nmarambi@dcc.uchile.cl>
2022-01-18 09:06:57 -0300

    client some functions deleted

--- src/client.rs
+++ src/client.rs
@@ -12,13 +12,12 @@ use crate::message::rdata::Rdata;
 use crate::message::DnsMessage;
 use crate::resolver::Resolver;
 
+use rand::{thread_rng, Rng};
 use std::collections::HashMap;
 use std::io::{Read, Write};
 use std::net::TcpStream;
 use std::net::UdpSocket;
-use rand::{thread_rng, Rng};
 use std::time::{Duration, Instant};
-
 
 pub fn run_client() {
     //Start timestamp
@@ -31,14 +30,8 @@ pub fn run_client() {
     let query_id: u16 = rng.gen();
 
     // Create query msg
-    let query_msg = DnsMessage::new_query_message(
-        HOST_NAME.to_string(),
-        QTYPE,
-        QCLASS,
-        0,
-        false,
-        query_id,
-    );
+    let query_msg =
+        DnsMessage::new_query_message(HOST_NAME.to_string(), QTYPE, QCLASS, 0, false, query_id);
 
     // Create response buffer
     let mut dns_message = DnsMessage::new();
@@ -53,10 +46,11 @@ pub fn run_client() {
         // Hashmap to save incomplete messages
         let mut messages = HashMap::<u16, DnsMessage>::new();
 
-        socket.set_read_timeout(Some(Duration::from_millis(TIMEOUT*1000)));
+        socket.set_read_timeout(Some(Duration::from_millis(TIMEOUT * 1000)));
 
         loop {
-            let response_result = Resolver::receive_udp_msg(socket.try_clone().unwrap(), messages.clone());
+            let response_result =
+                Resolver::receive_udp_msg(socket.try_clone().unwrap(), messages.clone());
             let messages_len = messages.len();
 
             match response_result {
@@ -87,7 +81,7 @@ pub fn run_client() {
 
         let full_msg = [&tcp_bytes_length, bytes.as_slice()].concat();
 
-        stream.set_read_timeout(Some(Duration::from_millis(TIMEOUT*1000)));
+        stream.set_read_timeout(Some(Duration::from_millis(TIMEOUT * 1000)));
 
         stream.write(&full_msg);
 
@@ -224,86 +218,4 @@ pub fn run_client() {
 
     let elapsed = now.elapsed();
     println!("Elapsed: {:.2?}", elapsed);
-}
-
-pub fn receive_udp_msg(
-    socket: UdpSocket,
-    mut messages: HashMap<u16, DnsMessage>,
-) -> Option<(DnsMessage, String)> {
-    let mut msg = [0; 512];
-    let (number_of_bytes_msg, address) = match socket.recv_from(&mut msg) {
-        Ok((bytes, addr)) => (bytes, addr.to_string()),
-        Err(e) => (0, "".to_string()),
-    };
-
-    if number_of_bytes_msg == 0 {
-        return None;
-    }
-
-    let dns_msg_parsed_result = DnsMessage::from_bytes(&msg);
-
-    match dns_msg_parsed_result {
-        Ok(_) => {}
-        Err(_) => {
-            return Some((DnsMessage::format_error_msg(), address));
-        }
-    };
-
-    let dns_msg_parsed = dns_msg_parsed_result.unwrap();
-
-    let query_id = dns_msg_parsed.get_query_id();
-    let trunc = dns_msg_parsed.get_header().get_tc();
-
-    match messages.get(&query_id) {
-        Some(val) => {
-            let mut msg = val.clone();
-            msg.add_answers(dns_msg_parsed.get_answer());
-            msg.add_authorities(dns_msg_parsed.get_authority());
-            msg.add_additionals(dns_msg_parsed.get_additional());
-
-            if trunc == true {
-                messages.insert(query_id, msg.clone());
-
-                return None;
-            } else {
-                msg.update_header_counters();
-                let mut header = msg.get_header();
-                header.set_tc(false);
-
-                msg.set_header(header);
-                messages.remove(&query_id);
-
-                return Some((msg.clone(), address));
-            }
-        }
-        None => {
-            if trunc == true {
-                messages.insert(query_id, dns_msg_parsed);
-                return None;
-            } else {
-                return Some((dns_msg_parsed, address));
-            }
-        }
-    }
-}
-
-pub fn receive_tcp_msg(mut stream: TcpStream) -> Option<Vec<u8>> {
-    let mut received_msg = [0; 2];
-    let number_of_bytes = stream.read(&mut received_msg).expect("No data received");
-
-    if number_of_bytes == 0 {
-        return None;
-    }
-
-    let mut tcp_msg_len = (received_msg[0] as u16) << 8 | received_msg[1] as u16;
-    let mut vec_msg: Vec<u8> = Vec::new();
-
-    while tcp_msg_len > 0 {
-        let mut msg = [0; 512];
-        let number_of_bytes_msg = stream.read(&mut msg).expect("Temporary Error");
-        tcp_msg_len = tcp_msg_len - number_of_bytes_msg as u16;
-        vec_msg.append(&mut msg.to_vec());
-    }
-
-    return Some(vec_msg);
 }

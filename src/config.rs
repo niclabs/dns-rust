12a1677148204ddc109aa778bb7f776db3452817
NelsonMQ <nmarambi@dcc.uchile.cl>
2022-01-27 16:17:14 -0300

    Resolver fixed

--- src/config.rs
+++ src/config.rs
@@ -10,15 +10,14 @@ pub static RESOLVER_IP_PORT: &'static str = "192.168.1.89:58396";
 pub static SBELT_ROOT_IPS: [&str; 3] = ["198.41.0.4", "199.9.14.201", "192.33.4.12"];
 
 // Queries quantity for each query, before the resolver panic in a Temporary Error
-pub static QUERIES_FOR_CLIENT_REQUEST: u16 = 20;
+pub static QUERIES_FOR_CLIENT_REQUEST: u16 = 50;
 
 // Uses cache or not
 pub static USE_CACHE: bool = true;
 // --------------------------------------------------
 
-
 // ------------- NameServer Config -------------------
 pub static NAME_SERVER_IP: &'static str = "192.168.1.89";
 pub static MASTER_FILES: [&str; 1] = ["test.txt"];
 pub static RECURSIVE_AVAILABLE: bool = true; // recursive name server available as default
-// ---------------------------------------------------
+                                             // ---------------------------------------------------

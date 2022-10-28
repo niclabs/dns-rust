942535c9356075e1b02be02266384e75e13d4929
kelly <kelly.andrea97@gmail.com>
2022-10-28 15:56:20 -0300

    fix: put unused channels back

--- src/resolver.rs
+++ src/resolver.rs
@@ -1180,20 +1180,22 @@ impl Resolver {
 
 pub fn run_resolver(resolver_ip_port: &'static str, sbelt_root_ips: [&str; 3], ns_data: HashMap<u16, HashMap<String, NSZone>>){
 
+    // Channels
     let (add_sender_udp, add_recv_udp) = mpsc::channel();
     let (delete_sender_udp, delete_recv_udp) = mpsc::channel();
     let (add_sender_tcp, add_recv_tcp) = mpsc::channel();
     let (delete_sender_tcp, delete_recv_tcp) = mpsc::channel();
-    let (add_sender_ns_udp, _) = mpsc::channel();
-    let (delete_sender_ns_udp, _) = mpsc::channel();
-    let (add_sender_ns_tcp, _) = mpsc::channel();
-    let (delete_sender_ns_tcp, _) = mpsc::channel();
+    let (add_sender_ns_udp, add_recv_ns_udp) = mpsc::channel();
+    let (delete_sender_ns_udp, delete_recv_ns_udp) = mpsc::channel();
+    let (add_sender_ns_tcp, add_recv_ns_tcp) = mpsc::channel();
+    let (delete_sender_ns_tcp, delete_recv_ns_tcp) = mpsc::channel();
     let (update_cache_sender_udp, rx_update_cache_udp) = mpsc::channel();
     let (update_cache_sender_tcp, rx_update_cache_tcp) = mpsc::channel();
-    let (update_cache_sender_ns_udp, _) = mpsc::channel();
-    let (update_cache_sender_ns_tcp, _) = mpsc::channel();
-    let (_, rx_update_zone_udp) = mpsc::channel();
-    let (_, rx_update_zone_tcp) = mpsc::channel();
+    let (update_cache_sender_ns_udp, rx_update_cache_ns_udp) = mpsc::channel();
+    let (update_cache_sender_ns_tcp, rx_update_cache_ns_tcp) = mpsc::channel();
+
+    let (update_zone_udp, rx_update_zone_udp) = mpsc::channel();
+    let (update_zone_tcp, rx_update_zone_tcp) = mpsc::channel();
 
     let mut resolver = Resolver::new(
         add_sender_udp.clone(),

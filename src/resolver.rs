1c489868d4e487a01b20a98a0523e6ee63f49a6b
Javi801 <javiera.alegria@live.cl>
2022-11-09 21:30:14 +0100

    fixed some warnings

--- src/resolver.rs
+++ src/resolver.rs
@@ -1185,17 +1185,17 @@ pub fn run_resolver(resolver_ip_port: &'static str, sbelt_root_ips: [&str; 3], n
     let (delete_sender_udp, delete_recv_udp) = mpsc::channel();
     let (add_sender_tcp, add_recv_tcp) = mpsc::channel();
     let (delete_sender_tcp, delete_recv_tcp) = mpsc::channel();
-    let (add_sender_ns_udp, add_recv_ns_udp) = mpsc::channel();
-    let (delete_sender_ns_udp, delete_recv_ns_udp) = mpsc::channel();
-    let (add_sender_ns_tcp, add_recv_ns_tcp) = mpsc::channel();
-    let (delete_sender_ns_tcp, delete_recv_ns_tcp) = mpsc::channel();
+    let (add_sender_ns_udp, _add_recv_ns_udp) = mpsc::channel();
+    let (delete_sender_ns_udp, _delete_recv_ns_udp) = mpsc::channel();
+    let (add_sender_ns_tcp, _add_recv_ns_tcp) = mpsc::channel();
+    let (delete_sender_ns_tcp, _delete_recv_ns_tcp) = mpsc::channel();
     let (update_cache_sender_udp, rx_update_cache_udp) = mpsc::channel();
     let (update_cache_sender_tcp, rx_update_cache_tcp) = mpsc::channel();
-    let (update_cache_sender_ns_udp, rx_update_cache_ns_udp) = mpsc::channel();
-    let (update_cache_sender_ns_tcp, rx_update_cache_ns_tcp) = mpsc::channel();
+    let (update_cache_sender_ns_udp, _rx_update_cache_ns_udp) = mpsc::channel();
+    let (update_cache_sender_ns_tcp, _rx_update_cache_ns_tcp) = mpsc::channel();
 
-    let (update_zone_udp, rx_update_zone_udp) = mpsc::channel();
-    let (update_zone_tcp, rx_update_zone_tcp) = mpsc::channel();
+    let (_update_zone_udp, rx_update_zone_udp) = mpsc::channel();
+    let (_update_zone_tcp, rx_update_zone_tcp) = mpsc::channel();
 
     let mut resolver = Resolver::new(
         add_sender_udp.clone(),
@@ -1235,7 +1235,8 @@ pub fn run_resolver(resolver_ip_port: &'static str, sbelt_root_ips: [&str; 3], n
     );
 }
 
-mod test {
+#[cfg(test)]
+mod resolver_test {
     use crate::dns_cache::DnsCache;
     use crate::domain_name::DomainName;
     use crate::message::rdata::a_rdata::ARdata;

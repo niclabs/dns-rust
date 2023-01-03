b15bc99e85c7bff91ffa235a32fb9cdc6a0b79b3
Ephy <vicente.ardiles@ug.uchile.cl>
2023-01-03 10:53:16 -0300

    config changes

--- src/client/config.rs
+++ src/client/config.rs
@@ -5,7 +5,7 @@ pub static CLIENT_IP_PORT: &'static str = "192.168.1.89:58397";
 
 /* Dns query configuration */
 
-pub static HOST_NAME: &'static str = "solotodo.cl";
+pub static HOST_NAME: &'static str = "dcc.uchile.cl";
 pub static QTYPE: u16 = 1;
 pub static QCLASS: u16 = 1;
 pub static TRANSPORT: &'static str = "TCP";
--- src/config.rs
+++ src/config.rs
@@ -11,14 +11,13 @@ pub static SBELT_ROOT_IPS: [&str; 3] = ["198.41.0.4", "199.9.14.201", "192.33.4.
 
 // Queries quantity for each query, before the resolver panic in a Temporary Error
 pub static QUERIES_FOR_CLIENT_REQUEST: u16 = 50;
-
 // Uses cache or not
 pub static USE_CACHE: bool = true;
 // --------------------------------------------------
 
 // ------------- NameServer Config -------------------
-pub static NAME_SERVER_IP: &'static str = "192.168.1.89";
-pub static MASTER_FILES: [&str; 1] = ["test.txt"];
+pub static NAME_SERVER_IP: &'static str = "127.0.0.1";
+pub static MASTER_FILES: [&str; 1] = ["1034-scenario-6.1-root.txt"];
 pub static RECURSIVE_AVAILABLE: bool = true; // recursive name server available as default
                                              // ---------------------------------------------------
 pub static CHECK_MASTER_FILES: bool = true; // checks validity of master files as default

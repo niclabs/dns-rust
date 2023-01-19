b90b9e244e4af73a764eebe6944fabc1ad206f8c
FranciscaOrtegaG <franciscaortega713@gmail.com>
2023-01-19 18:55:06 -0300

    changed for convention set_and get_retry

--- src/name_server/zone_refresh.rs
+++ src/name_server/zone_refresh.rs
@@ -305,7 +305,7 @@ mod zone_refresh_test {
         top_node.set_value(value);
         ns_zone.set_zone_nodes(top_node);
         let mut zone_refresh = ZoneRefresh::new(ns_zone);
-        
+
         assert_eq!(zone_refresh.get_refresh(), 0 as u32);
         zone_refresh.set_refresh(86400 as u32);
         assert_eq!(zone_refresh.get_refresh(), 86400 as u32);
@@ -313,17 +313,19 @@ mod zone_refresh_test {
 
     #[test]
     //TODO revisar práctica 1
-    fn set_and_get_retry_test(){
+    fn set_and_get_retry(){
         let mut ns_zone = NSZone::new();
         let mut value = Vec::<ResourceRecord>::new();
         let soa_rdata = Rdata::SomeSoaRdata(SoaRdata::new());
         let resource_record = ResourceRecord::new(soa_rdata);
         value.push(resource_record);
+
         ns_zone.get_zone_nodes().set_value(value.clone());
         let mut top_node = ns_zone.get_zone_nodes();
         top_node.set_value(value);
         ns_zone.set_zone_nodes(top_node);      
         let mut zone_refresh = ZoneRefresh::new(ns_zone);
+        
         assert_eq!(zone_refresh.get_retry(), 0 as u32);
         zone_refresh.set_retry(7200 as u32);
         assert_eq!(zone_refresh.get_retry(), 7200 as u32);

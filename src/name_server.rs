5615c7ac10da3c70d609d748e0e9fea85d01271b
Litr0 <gonzalo.sobarzoa@gmail.com>
2023-01-23 17:22:48 -0300

    test set and get zones by class

--- src/name_server.rs
+++ src/name_server.rs
@@ -2511,8 +2511,11 @@ impl NameServer {
 
 mod name_server_test{
     use std::sync::mpsc;
+    use crate::name_server::HashMap;
 
     use super::NameServer;
+    use crate::name_server::zone::NSZone;
+
 
     //ToDo: Revisar Práctica 1
     #[test]
@@ -2548,5 +2551,55 @@ mod name_server_test{
         assert!(name_server.primary_server);
         assert_eq!(name_server.queries_id_for_soa_rr.len(), 0);
         assert_eq!(name_server.refresh_zones_data.len(), 0);
+    }
+
+    //ToDo:Revisar Práctica 1
+    #[test]
+    fn set_and_get_zones_by_class(){
+        let (delete_sender_udp, _delete_recv_udp) = mpsc::channel();
+        let (delete_sender_tcp, _delete_recv_tcp) = mpsc::channel();
+        let (add_sender_ns_udp, _add_recv_ns_udp) = mpsc::channel();
+        let (add_sender_ns_tcp, _add_recv_ns_tcp) = mpsc::channel();
+        let (delete_sender_ns_udp, _delete_recv_ns_udp) = mpsc::channel();
+        let (delete_sender_ns_tcp, _delete_recv_ns_tcp) = mpsc::channel();
+        let (update_refresh_zone_udp, _rx_update_refresh_zone_udp) = mpsc::channel();
+        let (update_refresh_zone_tcp, _rx_update_refresh_zone_tcp) = mpsc::channel();
+        let (update_zone_udp_resolver, _tx_update_zone_udp_resolver) = mpsc::channel();
+        let (update_zone_tcp_resolver, _tx_update_zone_tcp_resolver) = mpsc::channel();
+
+        let mut name_server = NameServer::new(
+            true,
+            delete_sender_udp,
+            delete_sender_tcp,
+            add_sender_ns_udp,
+            delete_sender_ns_udp, 
+            add_sender_ns_tcp, 
+            delete_sender_ns_tcp, 
+            update_refresh_zone_udp,
+            update_refresh_zone_tcp,
+            update_zone_udp_resolver,
+            update_zone_tcp_resolver,
+        );
+
+        let file_name = "test.txt".to_string();
+        let origin = "example".to_string();
+        let ip = "192.80.24.11".to_string();
+        let nszone = NSZone::from_file(file_name, origin, ip, true);
+
+        let mut hash_string_and_nszone = HashMap::<String, NSZone>::new();
+
+        hash_string_and_nszone.insert("test.com".to_string(), nszone);
+
+        let mut new_ns_data = HashMap::<u16, HashMap<String, NSZone>>::new();
+
+        new_ns_data.insert(2, hash_string_and_nszone);
+
+        assert_eq!(name_server.get_zones_by_class().len(), 0);
+
+        name_server.set_zones_by_class(new_ns_data);
+
+        let res = name_server.get_zones_by_class();
+
+        assert_eq!(res.len(), 1);
     }
 }

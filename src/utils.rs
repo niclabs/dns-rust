c3aa91b9cc1625df937dadd09c21bdb2409d434f
Javi801 <javiera.alegria@live.cl>
2023-01-24 04:02:47 +0100

    Improved unit test of utils.rs

--- src/utils.rs
+++ src/utils.rs
@@ -72,100 +72,145 @@ mod utils_test {
     use super::check_label_name;
     use super::domain_validity_syntax;
 
-    // check_label_name Tests
     #[test]
-    fn check_empty_label_test() {
-        assert_eq!(check_label_name(String::from("")), false);
+    fn check_label_name_empty_label() {
+        let cln_empty_str = check_label_name(String::from(""));
+        assert_eq!(cln_empty_str, false);
     }
 
     #[test]
-    fn check_large_label_test() {
+    fn check_label_name_large_label() {
+        let cln_large_str = check_label_name(String::from(
+            "this-is-a-extremely-large-label-that-have-exactly--64-characters",
+        ));
+        assert_eq!(cln_large_str, false);
+    }
+
+    #[test]
+    fn check_label_name_first_label_character() {
+        let cln_symbol_str = check_label_name(String::from("-label"));
+        assert_eq!(cln_symbol_str, false);
+
+        let cln_num_str = check_label_name(String::from("0label"));
+        assert_eq!(cln_num_str, false);
+    }
+
+    #[test]
+    fn check_label_name_last_label_character() {
+        let cln_symbol_str = check_label_name(String::from("label-"));
+        assert_eq!(cln_symbol_str, false);
+
+        let cln_num_str = check_label_name(String::from("label2"));
+        assert_eq!(cln_num_str, true);
+    }
+
+    #[test]
+    fn check_label_name_interior_label_characters() {
+        let cln_dot_str = check_label_name(String::from("label.test"));
+        assert_eq!(cln_dot_str, false);
+
+        let cln_space_str = check_label_name(String::from("label test"));
+        assert_eq!(cln_space_str, false);
+    }
+
+    #[test]
+    fn check_label_name_valid_label() {
+        let cln_valid_str = check_label_name(String::from("label0test"));
+        assert_eq!(cln_valid_str, true);
+    }
+
+    #[test]
+    fn domain_validity_syntax_empty_dom() {
+        let ok = Ok(String::from(""));
+        let empty_dom = String::from("");
+
+        let empty_dom_validity = domain_validity_syntax(empty_dom);
+
+        assert_eq!(empty_dom_validity, ok);
+    }
+
+    #[test]
+    fn domain_validity_syntax_valid_dom() {
+        let ok = Ok(String::from("label1.label2."));
+        let valid_dom = String::from("label1.label2.");
+
+        let valid_dom_validity = domain_validity_syntax(valid_dom);
+
+        assert_eq!(valid_dom_validity, ok);
+    }
+
+    #[test]
+    fn domain_validity_syntax_wrong_middle_dom() {
+        let wrong_middle_dom = String::from("label1..label2");
+        let wrong_middle_dom_validity = domain_validity_syntax(wrong_middle_dom);
+
         assert_eq!(
-            check_label_name(String::from(
-                "this-is-a-extremely-large-label-that-have-exactly--64-characters"
-            )),
-            false
-        );
-    }
-
-    #[test]
-    fn check_first_label_character_test() {
-        assert_eq!(check_label_name(String::from("-label")), false);
-        assert_eq!(check_label_name(String::from("0label")), false);
-    }
-
-    #[test]
-    fn check_last_label_character_test() {
-        assert_eq!(check_label_name(String::from("label-")), false);
-        assert_eq!(check_label_name(String::from("label2")), true);
-    }
-
-    #[test]
-    fn check_interior_label_characters_test() {
-        assert_eq!(check_label_name(String::from("label.test")), false);
-        assert_eq!(check_label_name(String::from("label test")), false);
-    }
-
-    #[test]
-    fn check_valid_label_test() {
-        assert_eq!(check_label_name(String::from("label0test")), true);
-    }
-
-    // domain_validity_syntax Tests
-    #[test]
-    fn check_empty_domain_test() {
-        assert_eq!(
-            domain_validity_syntax(String::from("")),
-            Ok(String::from(""))
-        );
-        assert_eq!(
-            domain_validity_syntax(String::from("label1..label2")),
+            wrong_middle_dom_validity,
             Err("Error: Empty label is only allowed at the end of a hostname.")
         );
+    }
+
+    #[test]
+    fn domain_validity_syntax_wrong_init_dom() {
+        let wrong_init_dom = String::from(".label");
+        let wrong_init_dom_validity = domain_validity_syntax(wrong_init_dom);
+
         assert_eq!(
-            domain_validity_syntax(String::from(".label")),
+            wrong_init_dom_validity,
             Err("Error: Empty label is only allowed at the end of a hostname.")
         );
-        assert_eq!(
-            domain_validity_syntax(String::from("label1.label2.")),
-            Ok(String::from("label1.label2."))
-        );
     }
 
     #[test]
-    fn at_domain_name_validity_test() {
-        assert_eq!(
-            domain_validity_syntax(String::from("@")),
-            Ok(String::from("@"))
-        );
+    fn domain_validity_syntax_at_domain_name() {
+        let at_str = String::from("@");
+        let ok = Ok(at_str.clone());
+        let at_str_validity = domain_validity_syntax(at_str);
+
+        assert_eq!(at_str_validity, ok);
     }
 
     #[test]
-    fn syntactically_incorrect_domain_name_test() {
+    fn domain_validity_syntax_syntactically_incorrect_dom() {
+        let incorrect_dom = String::from("label1.2badlabel.test");
+        let incorrect_dom_validity = domain_validity_syntax(incorrect_dom);
+
         assert_eq!(
-            domain_validity_syntax(String::from("label1.2badlabel.test")),
+            incorrect_dom_validity,
             Err("Error: present domain name is not syntactically correct.")
         );
     }
 
     #[test]
-    fn syntactically_correct_domain_name_test() {
-        assert_eq!(
-            domain_validity_syntax(String::from("label1.label2.test")),
-            Ok(String::from("label1.label2.test"))
-        );
-        assert_eq!(
-            domain_validity_syntax(String::from("label1.label2.test.")),
-            Ok(String::from("label1.label2.test."))
-        );
+    fn domain_validity_syntax_syntactically_correct_dom() {
+        let correct_dom_1 = String::from("label1.label2.test");
+        let correct_dom_2 = String::from("label1.label2.test.");
+
+        let ok_dom_1 = Ok(correct_dom_1.clone());
+        let ok_dom_2 = Ok(correct_dom_2.clone());
+        let correct_dom_1_validity = domain_validity_syntax(correct_dom_1);
+        let correct_dom_2_validity = domain_validity_syntax(correct_dom_2);
+
+        assert_eq!(correct_dom_1_validity, ok_dom_1);
+        assert_eq!(correct_dom_2_validity, ok_dom_2);
     }
 
-    //ToDo: Revisar Práctica 1
     #[test]
-    fn is_reverse_query_test() {
-        assert!(!is_reverse_query(String::from("not_inverse.com")));
-        assert!(is_reverse_query(String::from("10.1.0.52")));
-        //clearly not an ip but the fn says true
-        //assert!(!is_reverse_query(String::from("100")));
+    fn is_reverse_query_dom() {
+        let dom_str = String::from("not_inverse.com");
+        assert_eq!(is_reverse_query(dom_str), false);
     }
+
+    #[test]
+    fn is_reverse_query_ip() {
+        let ip_str = String::from("10.1.0.52");
+        assert_eq!(is_reverse_query(ip_str), true);
+    }
+
+    #[test]
+    fn is_reverse_query_num(){
+        let num_str = String::from("100");
+        assert_eq!(is_reverse_query(num_str), false);
+    }
+
 }

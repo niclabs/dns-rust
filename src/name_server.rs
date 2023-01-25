a5efd7a533a0113facfd1d0705a27f61bb25b6c2
Litr0 <gonzalo.sobarzoa@gmail.com>
2023-01-25 13:12:49 -0300

    test add zone from master file

--- src/name_server.rs
+++ src/name_server.rs
@@ -3359,4 +3359,46 @@ mod name_server_test{
         assert_eq!(res_nszone.get_zone_nodes().get_name(), nsnode.clone().get_name());
         assert_eq!(res_nszone.get_ip_address_for_refresh_zone(), String::from("127.0.0.0"));
     }
+
+    //ToDo: Revisar Práctica 1
+    #[test]
+    fn add_zone_from_master_file(){
+        let (delete_sender_udp, _delete_recv_udp) = mpsc::channel();
+        let (delete_sender_tcp, _delete_recv_tcp) = mpsc::channel();
+        let (add_sender_ns_udp, _add_recv_ns_udp) = mpsc::channel();
+        let (add_sender_ns_tcp, _add_recv_ns_tcp) = mpsc::channel();
+        let (delete_sender_ns_udp, _delete_recv_ns_udp) = mpsc::channel();
+        let (delete_sender_ns_tcp, _delete_recv_ns_tcp) = mpsc::channel();
+        let (update_refresh_zone_udp, _rx_update_refresh_zone_udp) = mpsc::channel();
+        let (update_refresh_zone_tcp, _rx_update_refresh_zone_tcp) = mpsc::channel();
+        let (update_zone_udp_resolver, _tx_update_zone_udp_resolver) = mpsc::channel();
+        let (update_zone_tcp_resolver, _tx_update_zone_tcp_resolver) = mpsc::channel();
+
+        let mut name_server = NameServer::new(
+            true,
+            delete_sender_udp,
+            delete_sender_tcp,
+            add_sender_ns_udp,
+            delete_sender_ns_udp, 
+            add_sender_ns_tcp, 
+            delete_sender_ns_tcp, 
+            update_refresh_zone_udp,
+            update_refresh_zone_tcp,
+            update_zone_udp_resolver,
+            update_zone_tcp_resolver,
+        );
+
+        let file_name = "test.txt".to_string();
+        let origin = "example".to_string();
+        let ip = "192.80.24.11".to_string();
+
+        assert_eq!(name_server.get_zones_by_class().len(), 0);
+
+        name_server.add_zone_from_master_file(file_name, origin, ip, true);
+
+        let res = name_server.get_zones_by_class();
+
+        assert!(res.contains_key(&1));
+        assert_eq!(res.len(), 1);
+    }
 }

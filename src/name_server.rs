17ac49ab868d10f8376b7b47b30523d8f3e9e971
Litr0 <gonzalo.sobarzoa@gmail.com>
2023-01-25 17:36:13 -0300

    test fails search nearest ancestor zone in ns.rs

--- src/name_server.rs
+++ src/name_server.rs
@@ -3438,4 +3438,43 @@ mod name_server_test{
         assert!(!must_be_false);
         assert_eq!(_zone.get_ip_address_for_refresh_zone(), String::from(""));
     }
+
+    //ToDo: Completar test
+    #[test]
+    fn search_nearest_ancestor_zone(){
+        let (delete_sender_udp, _delete_recv_udp) = mpsc::channel();
+        let (delete_sender_tcp, _delete_recv_tcp) = mpsc::channel();
+        let (add_sender_ns_udp, _add_recv_ns_udp) = mpsc::channel();
+        let (add_sender_ns_tcp, _add_recv_ns_tcp) = mpsc::channel();
+        let (delete_sender_ns_udp, _delete_recv_ns_udp) = mpsc::channel();
+        let (delete_sender_ns_tcp, _delete_recv_ns_tcp) = mpsc::channel();
+        let (update_refresh_zone_udp, _rx_update_refresh_zone_udp) = mpsc::channel();
+        let (update_refresh_zone_tcp, _rx_update_refresh_zone_tcp) = mpsc::channel();
+        let (update_zone_udp_resolver, _tx_update_zone_udp_resolver) = mpsc::channel();
+        let (update_zone_tcp_resolver, _tx_update_zone_tcp_resolver) = mpsc::channel();
+
+        let mut name_server = NameServer::new(
+            true,
+            delete_sender_udp,
+            delete_sender_tcp,
+            add_sender_ns_udp,
+            delete_sender_ns_udp, 
+            add_sender_ns_tcp, 
+            delete_sender_ns_tcp, 
+            update_refresh_zone_udp,
+            update_refresh_zone_tcp,
+            update_zone_udp_resolver,
+            update_zone_tcp_resolver,
+        );
+
+        let file_name = "test.txt".to_string();
+        let origin = "example".to_string();
+        let ip = "192.80.24.11".to_string();
+
+        name_server.add_zone_from_master_file(file_name, origin, ip, true);
+
+        let zones_by_class = name_server.get_zones_by_class();
+
+        let (_zone, boolean) = NameServer::search_nearest_ancestor_zone(zones_by_class, String::from("uchile.cl"), 1);
+    }
 }

eb0c1301a1dd9c277d1a9f70461a18f3ca2038dd
Litr0 <gonzalo.sobarzoa@gmail.com>
2023-03-31 17:08:55 -0300

    test of example query fails

--- src/name_server.rs
+++ src/name_server.rs
@@ -3946,4 +3946,63 @@ mod name_server_test{
         assert_eq!(answer_rrs[0].get_name().get_name(), name.clone());
 
     }
+
+    //ToDo: Revisar
+    #[test]
+    #[ignore = "panic at label * does not exist"]
+    fn controlled_query_test(){
+        let (tx_delete_resolver_udp, _tx_resolver_recv_udp) = mpsc::channel::<(String, ResourceRecord)>();
+        let (tx_delete_resolver_tcp, _tx_resolver_recv_tcp) = mpsc::channel::<(String, ResourceRecord)>();
+        let (tx_delete_ns_udp, _tx_ns_recv_udp) = mpsc::channel::<(String, ResourceRecord)>();
+        let (tx_delete_ns_tcp, _tx_ns_recv_tcp) = mpsc::channel::<(String, ResourceRecord)>();
+
+        let cache = DnsCache::new();
+
+        let (delete_sender_udp, _delete_recv_udp) = mpsc::channel();
+        let (delete_sender_tcp, _delete_recv_tcp) = mpsc::channel();
+        let (add_sender_ns_udp, _add_recv_ns_udp) = mpsc::channel();
+        let (add_sender_ns_tcp, _add_recv_ns_tcp) = mpsc::channel();
+        let (delete_sender_ns_udp, _delete_recv_ns_udp) = mpsc::channel();
+        let (delete_sender_ns_tcp, _delete_recv_ns_tcp) = mpsc::channel();
+        let (update_refresh_zone_udp, _rx_update_refresh_zone_udp) = mpsc::channel();
+        let (update_refresh_zone_tcp, _rx_update_refresh_zone_tcp) = mpsc::channel();
+        let (update_zone_udp_resolver, _tx_update_zone_udp_resolver) = mpsc::channel();
+        let (update_zone_tcp_resolver, _tx_update_zone_tcp_resolver) = mpsc::channel();
+
+        let mut name_server = NameServer::new(
+            true,
+            delete_sender_udp,
+            delete_sender_tcp,
+            add_sender_ns_udp,
+            delete_sender_ns_udp, 
+            add_sender_ns_tcp, 
+            delete_sender_ns_tcp, 
+            update_refresh_zone_udp,
+            update_refresh_zone_tcp,
+            update_zone_udp_resolver,
+            update_zone_tcp_resolver,
+        );
+
+        let file_name = "test.txt".to_string();
+        let origin = "example".to_string();
+        let ip = "192.80.24.11".to_string();
+
+        name_server.add_zone_from_master_file(file_name, origin, ip, true);
+        let _zones_by_class = name_server.get_zones_by_class();
+
+        let dns_message = DnsMessage::new_query_message(String::from("dcc.uchile.cl."),
+                                                                     1, 
+                                                                     1, 
+                                                                     0, 
+                                                                     false, 
+                                                                     1);
+
+        let _response_message = NameServer::step_2(dns_message, 
+            _zones_by_class, 
+            cache, 
+            tx_delete_resolver_udp, 
+            tx_delete_resolver_tcp, 
+            tx_delete_ns_udp, 
+            tx_delete_ns_tcp); 
+    }
 }

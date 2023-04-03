c2910d68960694ef784d0396c68128f10dfdabd8
Litr0 <gonzalo.sobarzoa@gmail.com>
2023-04-03 17:46:14 -0400

    step_6_mx_with_aa_false

--- src/name_server.rs
+++ src/name_server.rs
@@ -4142,4 +4142,73 @@ mod name_server_test{
 
         assert_eq!(answer_msg.get_additional().len(), 0);
     }
+    
+    //ToDo: Revisar
+    //Question: The length should be 1 because in cache i have the data that should be added
+    #[test]
+    fn step_6_mx_with_aa_false(){
+        
+        let mut cache = DnsCache::new();
+        let name = String::from("dcc.uchile.cl.");
+        let a_rdata = ARdata::new();
+        let rdata = Rdata::SomeARdata(a_rdata);
+        let cache_rr = ResourceRecord::new(rdata);
+
+        cache.add(name.clone(), cache_rr);
+
+        let mut dns_message = DnsMessage::new_query_message(String::from("dcc.uchile.cl."),
+                                                                     1, 
+                                                                     1, 
+                                                                     0, 
+                                                                     false, 
+                                                                     1);
+
+        let mut new_answer = dns_message.get_answer();
+        let mut mx_rdata = MxRdata::new();
+        
+        let mut mx_name = mx_rdata.get_exchange();
+        mx_name.set_name(name);
+        mx_rdata.set_exchange(mx_name);
+
+        let rdata = Rdata::SomeMxRdata(mx_rdata);
+        let resource_record = ResourceRecord::new(rdata);
+        new_answer.push(resource_record);
+        dns_message.set_answer(new_answer);
+
+        let (delete_sender_udp, _delete_recv_udp) = mpsc::channel();
+        let (delete_sender_tcp, _delete_recv_tcp) = mpsc::channel();
+        let (add_sender_ns_udp, _add_recv_ns_udp) = mpsc::channel();
+        let (add_sender_ns_tcp, _add_recv_ns_tcp) = mpsc::channel();
+        let (delete_sender_ns_udp, _delete_recv_ns_udp) = mpsc::channel();
+        let (delete_sender_ns_tcp, _delete_recv_ns_tcp) = mpsc::channel();
+        let (update_refresh_zone_udp, _rx_update_refresh_zone_udp) = mpsc::channel();
+        let (update_refresh_zone_tcp, _rx_update_refresh_zone_tcp) = mpsc::channel();
+        let (update_zone_udp_resolver, _tx_update_zone_udp_resolver) = mpsc::channel();
+        let (update_zone_tcp_resolver, _tx_update_zone_tcp_resolver) = mpsc::channel();
+
+        let mut name_server = NameServer::new(
+            true,
+            delete_sender_udp,
+            delete_sender_tcp,
+            add_sender_ns_udp,
+            delete_sender_ns_udp, 
+            add_sender_ns_tcp, 
+            delete_sender_ns_tcp, 
+            update_refresh_zone_udp,
+            update_refresh_zone_tcp,
+            update_zone_udp_resolver,
+            update_zone_tcp_resolver,
+        );
+
+        let file_name = "test.txt".to_string();
+        let origin = "example".to_string();
+        let ip = "192.80.24.11".to_string();
+
+        name_server.add_zone_from_master_file(file_name, origin, ip, true);
+        let _zones_by_class = name_server.get_zones_by_class();
+
+        let answer_msg = NameServer::step_6(dns_message, cache, _zones_by_class);
+
+        assert_eq!(answer_msg.get_additional().len(), 0);
+    }
 }

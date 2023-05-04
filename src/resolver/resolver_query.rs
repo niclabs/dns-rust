6f9fedeaf4e70f6255f196b4e491e107113ffeda
Katia Fredes <katia.fredes@ug.uchile.cl>
2023-05-04 19:53:51 -0400

    fix step_4a_aa_false_no_cache() test

--- src/resolver/resolver_query.rs
+++ src/resolver/resolver_query.rs
@@ -4829,88 +4829,76 @@ mod resolver_query_tests {
         );
     }
 
-    // #[test]
-    // fn step_4a_aa_false_no_cache() {
-    //     // Channels
-    //     let (add_sender_udp, _add_recv_udp) = mpsc::channel();
-    //     let (delete_sender_udp, _delete_recv_udp) = mpsc::channel();
-    //     let (add_sender_tcp, _add_recv_tcp) = mpsc::channel();
-    //     let (delete_sender_tcp, _delete_recv_tcp) = mpsc::channel();
-    //     let (add_sender_ns_udp, _add_recv_ns_udp) = mpsc::channel();
-    //     let (delete_sender_ns_udp, _delete_recv_ns_udp) = mpsc::channel();
-    //     let (add_sender_ns_tcp, _add_recv_ns_tcp) = mpsc::channel();
-    //     let (delete_sender_ns_tcp, _delete_recv_ns_tcp) = mpsc::channel();
-    //     let (tx_update_query, _rx_update_query) = mpsc::channel();
-    //     let (tx_delete_query, _rx_delete_query) = mpsc::channel();
-    //     let (tx_update_cache_udp, _rx_update_cache_udp) = mpsc::channel();
-    //     let (tx_update_cache_tcp, _rx_update_cache_tcp) = mpsc::channel();
-    //     let (tx_update_cache_ns_udp, _rx_update_cache_ns_udp) = mpsc::channel();
-    //     let (tx_update_cache_ns_tcp, _rx_update_cache_ns_tcp) = mpsc::channel();
-    //     let (tx_update_slist_tcp, _rx_update_slist_tcp) = mpsc::channel();
-    //     let (tx_update_self_slist, _rx_update_self_slist) = mpsc::channel();
-    //     let mut resolver_query = ResolverQuery::new(
-    //         add_sender_udp,
-    //         delete_sender_udp,
-    //         add_sender_tcp,
-    //         delete_sender_tcp,
-    //         add_sender_ns_udp,
-    //         delete_sender_ns_udp,
-    //         add_sender_ns_tcp,
-    //         delete_sender_ns_tcp,
-    //         tx_update_query,
-    //         tx_delete_query,
-    //         DnsMessage::new(),
-    //         tx_update_cache_udp,
-    //         tx_update_cache_tcp,
-    //         tx_update_cache_ns_udp,
-    //         tx_update_cache_ns_tcp,
-    //         tx_update_slist_tcp,
-    //         tx_update_self_slist,
-    //     );
-    //     resolver_query.set_sname("*test.com".to_string());
-    //     resolver_query.set_rd(true);
-    //     resolver_query.set_stype(1);
-    //     resolver_query.set_sclass(1);
-    //     let mut dns_message = resolver_query.create_query_message();
-    //     let mut a_rdata_1 = ARdata::new();
-    //     let mut a_rdata_2 = ARdata::new();
-    //     let ip_address_1: [u8; 4] = [127, 0, 0, 0];
-    //     let ip_address_2: [u8; 4] = [127, 0, 7, 0];
-    //     a_rdata_1.set_address(ip_address_1);
-    //     a_rdata_2.set_address(ip_address_2);
-    //     let rdata_1 = Rdata::SomeARdata(a_rdata_1);
-    //     let rdata_2 = Rdata::SomeARdata(a_rdata_2);
-    //     let mut rr_1 = ResourceRecord::new(rdata_1);
-    //     let mut rr_2 = ResourceRecord::new(rdata_2);
-    //     rr_1.set_class(1 as u16);
-    //     rr_1.set_type_code(1);
-    //     rr_1.set_ttl(888);
-    //     rr_2.set_class(2 as u16);
-    //     let mut rr_vec_1 = Vec::<ResourceRecord>::new();
-    //     rr_vec_1.push(rr_1.clone());
-    //     let mut rr_vec_2 = Vec::<ResourceRecord>::new();
-    //     rr_vec_2.push(rr_2.clone());
-    //     dns_message.add_answers(rr_vec_1);
-    //     dns_message.add_answers(rr_vec_2);
-    //     let mut header = dns_message.get_header();
-    //     header.set_rcode(0);
-    //     header.set_aa(false);
-    //     let mut question = dns_message.get_question();
-    //     let mut qname = question.get_qname();
-    //     qname.set_name("nname.com".to_string());
-    //     question.set_qname(qname);
-    //     dns_message.set_question(question);
-    //     dns_message.set_header(header);
+    #[test]
+    fn step_4a_aa_false_no_cache() {
+        // Channels
+        let (add_sender_udp, _add_recv_udp) = mpsc::channel();
+        let (delete_sender_udp, _delete_recv_udp) = mpsc::channel();
+        let (add_sender_tcp, _add_recv_tcp) = mpsc::channel();
+        let (delete_sender_tcp, _delete_recv_tcp) = mpsc::channel();
+        let (tx_update_query, _rx_update_query) = mpsc::channel();
+        let (tx_delete_query, _rx_delete_query) = mpsc::channel();
+        let (tx_update_cache_udp, _rx_update_cache_udp) = mpsc::channel();
+        let (tx_update_cache_tcp, _rx_update_cache_tcp) = mpsc::channel();
+        let (tx_update_slist_tcp, _rx_update_slist_tcp) = mpsc::channel();
+        let (tx_update_self_slist, _rx_update_self_slist) = mpsc::channel();
+        let mut resolver_query = ResolverQuery::new(
+            add_sender_udp,
+            delete_sender_udp,
+            add_sender_tcp,
+            delete_sender_tcp,
+            tx_update_query,
+            tx_delete_query,
+            DnsMessage::new(),
+            tx_update_cache_udp,
+            tx_update_cache_tcp,
+            tx_update_slist_tcp,
+            tx_update_self_slist,
+        );
+        resolver_query.set_sname("*test.com".to_string());
+        resolver_query.set_rd(true);
+        resolver_query.set_stype(1);
+        resolver_query.set_sclass(1);
+        let mut dns_message = resolver_query.create_query_message();
+        let mut a_rdata_1 = ARdata::new();
+        let mut a_rdata_2 = ARdata::new();
+        let ip_address_1: [u8; 4] = [127, 0, 0, 0];
+        let ip_address_2: [u8; 4] = [127, 0, 7, 0];
+        a_rdata_1.set_address(ip_address_1);
+        a_rdata_2.set_address(ip_address_2);
+        let rdata_1 = Rdata::SomeARdata(a_rdata_1);
+        let rdata_2 = Rdata::SomeARdata(a_rdata_2);
+        let mut rr_1 = ResourceRecord::new(rdata_1);
+        let mut rr_2 = ResourceRecord::new(rdata_2);
+        rr_1.set_class(1 as u16);
+        rr_1.set_type_code(1);
+        rr_1.set_ttl(888);
+        rr_2.set_class(2 as u16);
+        let mut rr_vec_1 = Vec::<ResourceRecord>::new();
+        rr_vec_1.push(rr_1.clone());
+        let mut rr_vec_2 = Vec::<ResourceRecord>::new();
+        rr_vec_2.push(rr_2.clone());
+        dns_message.add_answers(rr_vec_1);
+        dns_message.add_answers(rr_vec_2);
+        let mut header = dns_message.get_header();
+        header.set_rcode(0);
+        header.set_aa(false);
+        let mut question = dns_message.get_question();
+        let mut qname = question.get_qname();
+        qname.set_name("nname.com".to_string());
+        question.set_qname(qname);
+        dns_message.set_question(question);
+        dns_message.set_header(header);
 
-    //     let msg = resolver_query.step_4a(dns_message); // fail in exist cache when the name searched doesn't contains a "*"
-    //     assert_eq!(msg.get_header().get_rd(), true);
-    //     assert_eq!(msg.get_question().get_qtype(), 1);
-    //     assert_eq!(msg.get_question().get_qclass(), 1);
-    //     assert_eq!(
-    //         msg.get_question().get_qname().get_name(),
-    //         "nname.com".to_string()
-    //     );
-    // }
+        let msg = resolver_query.step_4a(dns_message); // fail in exist cache when the name searched doesn't contains a "*"
+        assert_eq!(msg.get_header().get_rd(), true);
+        assert_eq!(msg.get_question().get_qtype(), 1);
+        assert_eq!(msg.get_question().get_qclass(), 1);
+        assert_eq!(
+            msg.get_question().get_qname().get_name(),
+            "nname.com".to_string()
+        );
+    }
 
     // #[test]
     // fn step_4a_aa_false_with_cache() {

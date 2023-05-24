f5062737b1b15cdb780981b0ff34f88a9111b9f2
Litr0 <gonzalo.sobarzoa@gmail.com>
2023-05-24 11:50:05 -0400

    change list type in run resolver for testing

--- tests/common/mod.rs
+++ tests/common/mod.rs
@@ -11,7 +11,7 @@ use dns_rust::{
 
 
 #[allow(dead_code)]
-pub fn run_resolver_for_testing(resolver_ip_port: &str,sbelt_root_ips: [&str;3]) {
+pub fn run_resolver_for_testing(resolver_ip_port: &str,sbelt_root_ips: &'static [&'static str]) {
     // Channels
     let (add_sender_udp, add_recv_udp) = mpsc::channel();
     let (delete_sender_udp, delete_recv_udp) = mpsc::channel();

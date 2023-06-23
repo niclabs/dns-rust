98006503654064cb30bffac989d9a767efe01278
Katia Fredes <katia.fredes@ug.uchile.cl>
2023-06-23 15:36:54 -0400

    Docs: fix docum in rr according to convention

--- src/message/resource_record.rs
+++ src/message/resource_record.rs
@@ -7,73 +7,76 @@ use std::fmt;
 use std::vec::Vec;
 
 #[derive(Clone, PartialEq, Debug)]
-// An struct that represents the resource record secction from a dns message
-//                               1  1  1  1  1  1
-// 0  1  2  3  4  5  6  7  8  9  0  1  2  3  4  5
-// +--+--+--+--+--+--+--+--+--+--+--+--+--+--+--+--+
-// |                                               |
-// /                                               /
-// /                      NAME                     /
-// |                                               |
-// +--+--+--+--+--+--+--+--+--+--+--+--+--+--+--+--+
-// |                      TYPE                     |
-// +--+--+--+--+--+--+--+--+--+--+--+--+--+--+--+--+
-// |                     CLASS                     |
-// +--+--+--+--+--+--+--+--+--+--+--+--+--+--+--+--+
-// |                      TTL                      |
-// |                                               |
-// +--+--+--+--+--+--+--+--+--+--+--+--+--+--+--+--+
-// |                   RDLENGTH                    |
-// +--+--+--+--+--+--+--+--+--+--+--+--+--+--+--+--|
-// /                     RDATA                     /
-// /                                               /
-// +--+--+--+--+--+--+--+--+--+--+--+--+--+--+--+--+
-//
+/// An struct that represents the Resource Record secction from a dns message.
+/// 
+/// ```text
+/// The Resource Record is composed by:
+///                               1  1  1  1  1  1
+/// 0  1  2  3  4  5  6  7  8  9  0  1  2  3  4  5
+/// +--+--+--+--+--+--+--+--+--+--+--+--+--+--+--+--+
+/// |                                               |
+/// /                                               /
+/// /                      NAME                     /
+/// |                                               |
+/// +--+--+--+--+--+--+--+--+--+--+--+--+--+--+--+--+
+/// |                      TYPE                     |
+/// +--+--+--+--+--+--+--+--+--+--+--+--+--+--+--+--+
+/// |                     CLASS                     |
+/// +--+--+--+--+--+--+--+--+--+--+--+--+--+--+--+--+
+/// |                      TTL                      |
+/// |                                               |
+/// +--+--+--+--+--+--+--+--+--+--+--+--+--+--+--+--+
+/// |                   RDLENGTH                    |
+/// +--+--+--+--+--+--+--+--+--+--+--+--+--+--+--+--|
+/// /                     RDATA                     /
+/// /                                               /
+/// +--+--+--+--+--+--+--+--+--+--+--+--+--+--+--+--+
+/// ```
 pub struct ResourceRecord {
-    // Domain Name
+    /// Domain Name
     name: DomainName,
-    // Specifies the meaning of the data in the RDATA
+    /// Specifies the meaning of the data in the RDATA.
     rtype: Rtype,
-    // Specifies the class of the data in the RDATA
+    /// Specifies the class of the data in the RDATA.
     class: Rclass,
-    // Specifies the time interval (in seconds) that the resource record may be cached before it should be discarded.
+    /// Specifies the time interval (in seconds) that the resource record may be cached before it should be discarded.
     ttl: u32,
-    // Specifies the length in octets of the RDATA field
+    /// Specifies the length in octets of the RDATA field.
     rdlength: u16,
-    // The format of this information varies according to the TYPE and CLASS of the resource record
+    /// The format of this information varies according to the TYPE and CLASS of the resource record.
     rdata: Rdata,
 }
 
-// Trait to convert struct in bytes
+/// Trait to convert struct in a vector of bytes.
 pub trait ToBytes {
+    /// Converts struct in a vector of bytes.
     fn to_bytes(&self) -> Vec<u8>;
 }
 
-// Trait to create an struct from bytes
+/// Trait to create struct from bytes and full message.
 pub trait FromBytes<T> {
+    /// Creates struct from bytes and full message.
     fn from_bytes(bytes: &[u8], full_msg: &[u8]) -> T;
 }
 
 // Methods
 impl ResourceRecord {
-    /* Given a rdata, creates a new ResourceRecord with default values and the rdata.
-     # Examples
-     ```
-     let txt_rdata = Rdata::SomeTxtRdata(TxtRdata::new(String::from("dcc")));
-     let mut resource_record = ResourceRecord::new(txt_rdata);
-
-     assert_eq!(resource_record.name.get_name(), String::from(""));
-     assert_eq!(resource_record.rtype, 0);
-     assert_eq!(resource_record.class, 0);
-     assert_eq!(resource_record.ttl, 0);
-     assert_eq!(resource_record.rdlength, 0);
-     assert_eq!(
-        resource_record.rdata.unwrap().get_text(),
-        String::from("dcc")
-     );
-     ```
-    */
-
+    /// Given a `Rdata`, creates a new `ResourceRecord` with default values and the `Rdata`.
+    /// 
+    ///  # Examples
+    ///  ```
+    ///  let txt_rdata = Rdata::SomeTxtRdata(TxtRdata::new(String::from("dcc")));
+    ///  let mut resource_record = ResourceRecord::new(txt_rdata);
+    ///  assert_eq!(resource_record.name.get_name(), String::from(""));
+    ///  assert_eq!(resource_record.rtype, 0);
+    ///  assert_eq!(resource_record.class, 0);
+    ///  assert_eq!(resource_record.ttl, 0);
+    ///  assert_eq!(resource_record.rdlength, 0);
+    ///  assert_eq!(
+    ///     resource_record.rdata.unwrap().get_text(),
+    ///     String::from("dcc")
+    ///  );
+    ///  ```
     pub fn new(rdata: Rdata) -> ResourceRecord {
         match rdata {
             Rdata::SomeARdata(val) => ResourceRecord {
@@ -165,27 +168,27 @@ impl ResourceRecord {
         resource_record
     }*/
 
-    /* Given an array of bytes, creates a new ResourceRecord
-     # Examples
-     ```
-     let bytes_msg: [u8; 23] = [
-         3, 100, 99, 99, 2, 99, 108, 0, 0, 16, 0, 1, 0, 0, 0b00010110, 0b00001010, 0, 5, 104,
-         101, 108, 108, 111,
-     ];
+    /// Given an array of bytes, creates a new `ResourceRecord`.
+    /// 
+    /// # Examples
+    ///  ```
+    ///  let bytes_msg: [u8; 23] = [
+    ///      3, 100, 99, 99, 2, 99, 108, 0, 0, 16, 0, 1, 0, 0, 0b00010110, 0b00001010, 0, 5, 104,
+    ///      101, 108, 108, 111,
+    ///  ];
 
-     let resource_record_test = ResourceRecord::<Rdata>::from_bytes(&bytes_msg);
+    ///  let resource_record_test = ResourceRecord::<Rdata>::from_bytes(&bytes_msg);
 
-     assert_eq!(resource_record_test.get_name().get_name(), String::from("dcc.cl"));
-     assert_eq!(resource_record_test.get_rtype(), 16);
-     assert_eq!(resource_record_test.get_class(), 1);
-     assert_eq!(resource_record_test.get_ttl(), 5642);
-     assert_eq!(resource_record_test.get_rdlength(), 5);
-     assert_eq!(
-         resource_record_test.get_rdata().unwrap().get_text(),
-         String::from("hello")
-     );
-     ```
-    */
+    ///  assert_eq!(resource_record_test.get_name().get_name(), String::from("dcc.cl"));
+    ///  assert_eq!(resource_record_test.get_rtype(), 16);
+    ///  assert_eq!(resource_record_test.get_class(), 1);
+    ///  assert_eq!(resource_record_test.get_ttl(), 5642);
+    ///  assert_eq!(resource_record_test.get_rdlength(), 5);
+    ///  assert_eq!(
+    ///      resource_record_test.get_rdata().unwrap().get_text(),
+    ///      String::from("hello")
+    ///  );
+    ///  ```
     pub fn from_bytes<'a>(
         bytes: &'a [u8],
         full_msg: &'a [u8],
@@ -248,7 +251,7 @@ impl ResourceRecord {
         Ok((resource_record, &bytes_without_name[end_rr_byte..]))
     }
 
-    // Returns a byte that represents the first byte from type code in the dns message.
+    /// Returns a byte that represents the first byte from type code in the dns message.
     fn get_first_type_code_byte(&self) -> u8 {
         let type_code = Rtype::from_rtype_to_int(self.get_rtype());
         let first_byte = (type_code >> 8) as u8;
@@ -256,7 +259,7 @@ impl ResourceRecord {
         first_byte
     }
 
-    // Returns a byte that represents the second byte from type code in the dns message.
+    /// Returns a byte that represents the second byte from type code in the dns message.
     fn get_second_type_code_byte(&self) -> u8 {
         let type_code = Rtype::from_rtype_to_int(self.get_rtype());
         let second_byte = type_code as u8;
@@ -264,7 +267,7 @@ impl ResourceRecord {
         second_byte
     }
 
-    // Returns a byte that represents the first byte from class in the dns message.
+    /// Returns a byte that represents the first byte from class in the dns message.
     fn get_first_class_byte(&self) -> u8 {
         let class = Rclass::from_rclass_to_int(self.get_class());
         let first_byte = (class >> 8) as u8;
@@ -272,7 +275,7 @@ impl ResourceRecord {
         first_byte
     }
 
-    // Returns a byte that represents the second byte from class in the dns message.
+    /// Returns a byte that represents the second byte from class in the dns message.
     fn get_second_class_byte(&self) -> u8 {
         let class = Rclass::from_rclass_to_int(self.get_class());
         let second_byte = class as u8;
@@ -280,7 +283,7 @@ impl ResourceRecord {
         second_byte
     }
 
-    // Returns a byte that represents the first byte from ttl in the dns message.
+    /// Returns a byte that represents the first byte from ttl in the dns message.
     fn get_first_ttl_byte(&self) -> u8 {
         let ttl = self.get_ttl();
         let first_byte = (ttl >> 24) as u8;
@@ -288,7 +291,7 @@ impl ResourceRecord {
         first_byte
     }
 
-    // Returns a byte that represents the second byte from ttl in the dns message.
+    /// Returns a byte that represents the second byte from ttl in the dns message.
     fn get_second_ttl_byte(&self) -> u8 {
         let ttl = self.get_ttl();
         let second_byte = (ttl >> 16) as u8;
@@ -296,7 +299,7 @@ impl ResourceRecord {
         second_byte
     }
 
-    // Returns a byte that represents the third byte from ttl in the dns message.
+    /// Returns a byte that represents the third byte from ttl in the dns message.
     fn get_third_ttl_byte(&self) -> u8 {
         let ttl = self.get_ttl();
         let third_byte = (ttl >> 8) as u8;
@@ -304,7 +307,7 @@ impl ResourceRecord {
         third_byte
     }
 
-    // Returns a byte that represents the fourth byte from ttl in the dns message.
+    /// Returns a byte that represents the fourth byte from ttl in the dns message.
     fn get_fourth_ttl_byte(&self) -> u8 {
         let ttl = self.get_ttl();
         let fourth_byte = ttl as u8;
@@ -312,7 +315,7 @@ impl ResourceRecord {
         fourth_byte
     }
 
-    // Returns a byte that represents the first byte from rdlength in the dns message.
+    /// Returns a byte that represents the first byte from rdlength in the dns message.
     #[allow(dead_code)]
     fn get_first_rdlength_byte(&self) -> u8 {
         let rdlength = self.get_rdlength();
@@ -321,7 +324,7 @@ impl ResourceRecord {
         first_byte
     }
 
-    // Returns a byte that represents the second byte from rdlength in the dns message.
+    /// Returns a byte that represents the second byte from rdlength in the dns message.
     #[allow(dead_code)]
     fn get_second_rdlength_byte(&self) -> u8 {
         let rdlength = self.get_rdlength();
@@ -330,43 +333,42 @@ impl ResourceRecord {
         second_byte
     }
 
-    // Returns a vec of bytes that represents the rdata in the dns message.
+    /// Returns a vec of bytes that represents the rdata in the dns message.
     fn rdata_to_bytes(&self) -> Vec<u8> {
         let rdata = self.get_rdata();
 
         rdata.to_bytes()
     }
 
-    /* Returns a vec fo bytes that represents the resource record
+    /// Returns a `Vec<u8>` of bytes that represent the resource record in the dns message.
+    /// 
+    /// # Example
+    ///  ```
+    ///  let txt_rdata = Rdata::SomeTxtRdata(TxtRdata::new(String::from("dcc")));
+    ///  let mut resource_record = ResourceRecord::new(txt_rdata);
+    ///  let mut domain_name = DomainName::new();
+    ///  domain_name.set_name(String::from("dcc.cl"));
 
-     # Example
-     ```
-     let txt_rdata = Rdata::SomeTxtRdata(TxtRdata::new(String::from("dcc")));
-     let mut resource_record = ResourceRecord::new(txt_rdata);
-     let mut domain_name = DomainName::new();
-     domain_name.set_name(String::from("dcc.cl"));
+    ///  resource_record.set_name(domain_name);
+    ///  resource_record.set_type_code(2);
+    ///  resource_record.set_class(1);
+    ///  resource_record.set_ttl(5642);
+    ///  resource_record.set_rdlength(3);
 
-     resource_record.set_name(domain_name);
-     resource_record.set_type_code(2);
-     resource_record.set_class(1);
-     resource_record.set_ttl(5642);
-     resource_record.set_rdlength(3);
+    ///  let bytes_msg = [
+    ///      3, 100, 99, 99, 2, 99, 108, 0, 0, 2, 0, 1, 0, 0, 0b00010110, 0b00001010, 0, 3, 100, 99,
+    ///     99,
+    ///  ];
 
-     let bytes_msg = [
-         3, 100, 99, 99, 2, 99, 108, 0, 0, 2, 0, 1, 0, 0, 0b00010110, 0b00001010, 0, 3, 100, 99,
-        99,
-     ];
+    ///  let rr_to_bytes = resource_record.to_bytes();
 
-     let rr_to_bytes = resource_record.to_bytes();
+    ///  let mut i = 0;
 
-     let mut i = 0;
-
-     for value in rr_to_bytes.as_slice() {
-        assert_eq!(*value, bytes_msg[i]);
-         i += 1;
-     }
-     ```
-    */
+    ///  for value in rr_to_bytes.as_slice() {
+    ///     assert_eq!(*value, bytes_msg[i]);
+    ///      i += 1;
+    ///  }
+    ///  ```
     pub fn to_bytes(&self) -> Vec<u8> {
         let mut rr_bytes: Vec<u8> = Vec::new();
 
@@ -404,38 +406,39 @@ impl ResourceRecord {
     }
 }
 
-// Setters
+/// Setters
 impl ResourceRecord {
-    // Sets the ame attribute with a value
+    /// Sets the ame attribute with a value.
     pub fn set_name(&mut self, name: DomainName) {
         self.name = name;
     }
 
-    // Sets the type_code attribute with a value
+    /// Sets the type_code attribute with a value.
     pub fn set_type_code(&mut self, rtype: Rtype) {
         self.rtype = rtype;
     }
 
-    // Sets the class attribute with a value
+    /// Sets the class attribute with a value.
     pub fn set_class(&mut self, class: Rclass) {
         self.class = class;
     }
 
-    // Sets the ttl attribute with a value
+    /// Sets the ttl attribute with a value.
     pub fn set_ttl(&mut self, ttl: u32) {
         self.ttl = ttl;
     }
 
-    // Sets the rdlength attribute with a value
+    /// Sets the rdlength attribute with a value.
     pub fn set_rdlength(&mut self, rdlength: u16) {
         self.rdlength = rdlength;
     }
 
-    // Sets the rdata attribute with a value
+    /// Sets the rdata attribute with a value.
     pub fn set_rdata(&mut self, rdata: Rdata) {
         self.rdata = rdata.clone();
     }
 }
+
 impl ResourceRecord {
     pub fn rr_equal(&mut self, rr: ResourceRecord) -> bool {
         let a: u16 = Rtype::from_rtype_to_int(self.get_rtype());
@@ -463,34 +466,34 @@ impl ResourceRecord {
     }
 }
 
-// Getters
+/// Getters
 impl ResourceRecord {
-    // Gets the name attribute value
+    /// Returns a copy of the name attribute value
     pub fn get_name(&self) -> DomainName {
         self.name.clone()
     }
 
-    // Gets the type_code attribute value
+    /// Returns a copy of the `rtype` attribute value.
     pub fn get_rtype(&self) -> Rtype {
         self.rtype.clone()
     }
 
-    // Gets the class attribute value
+    /// Returns a copy of the class attribute value.
     pub fn get_class(&self) -> Rclass {
         self.class.clone()
     }
 
-    // Gets the ttl attribute value
+    //// Returns a copy of the ttl attribute value.
     pub fn get_ttl(&self) -> u32 {
         self.ttl.clone()
     }
 
-    // Gets the rdlength attribute value
+    /// Returns a copy of the rdlength attribute value.
     pub fn get_rdlength(&self) -> u16 {
         self.rdlength.clone()
     }
 
-    // Gets the rdata attribute value
+    /// Returns a copy of the rdata attribute value.
     pub fn get_rdata(&self) -> Rdata {
         self.rdata.clone()
     }

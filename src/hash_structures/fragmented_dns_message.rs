40e18b918e01204bbfbd9944aba89d9a0e721cc3
Katia Fredes <katia.fredes@ug.uchile.cl>
2023-07-10 16:41:39 -0400

    add: fragmented_dns_messages functions

--- src/hash_structures/fragmented_dns_message.rs
+++ src/hash_structures/fragmented_dns_message.rs
@@ -9,4 +9,36 @@ pub struct FragmentedDnsMessage {
 
 }
 
-impl FragmentedDnsMessage {}
+impl FragmentedDnsMessage {
+    fn new() -> Self {
+        FragmentedDnsMessage {
+            messages: HashMap::new(),
+        }
+    }
+
+    fn add_message(&mut self, msg_id: id, dns_message: DnsMessage) {
+        self.messages.insert(msg_id, dns_message);
+    }
+
+    fn remove_message(&mut self, msg_id: &id) -> Option<DnsMessage> {
+        self.messages.remove(msg_id)
+    }
+
+    fn get_dns_message(&self, msg_id: &id) -> Option<&DnsMessage> {
+        self.messages.get(key)
+    }
+
+    fn get_messages(&self) -> HashMap<id, DnsMessage> {
+        self.messages.clone()
+    }
+}
+
+impl FragmentedDnsMessage {
+    fn set_messages(&mut self, messages: HashMap<id, DnsMessage>) {
+        self.messages = messages;
+    }
+
+    fn set_dns_message(&mut self, msg_id: id, dns_message: DnsMessage) {
+        self.messages.insert(msg_id, dns_message);
+    }
+}

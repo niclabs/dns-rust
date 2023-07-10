329c09d8fd5eabd68d525c3e233b14b221a6f199
Litr0 <gonzalo.sobarzoa@gmail.com>
2023-07-10 12:14:20 -0400

    add to cache data fn ready

--- src/hash_structures/cache_data.rs
+++ src/hash_structures/cache_data.rs
@@ -34,20 +34,28 @@ impl CacheData{
     /// # Example
     /// ```
     /// let mut cache_data = CacheData::new();
-    /// let mut host_data = HostData::new();
-    /// let txt_rdata = Rdata::SomeTxtRdata(TxtRdata::new(vec!["dcc".to_string()]));
-    /// let mut resource_record = ResourceRecord::new(txt_rdata);
-    /// let rtype = resource_record.get_rtype();
-    /// cache_data.add_to_cache_data(rtype,host_data);
+    /// let rr_cache = RRCache::new();
+    /// cache_data.add_to_cache_data(Rtype::A, String::from("uchile.cl"), rr_cache);
     /// ```
-    /// 
     /// # Arguments
-    /// * `rtype`  -An enum that indicates the type of the data
-    /// * `host_data` -The structure HostData that contains host name and host cache
+    /// * `rtype` - A Rtype that represents the rtype of the cache data
+    /// * `domain_name` - A String that represents the domain name of the cache data
+    /// * `rr_cache` - A RRCache that represents the rr_cache of the cache data
 
-    pub fn add_to_cache_data(&mut self, rtype: Rtype, host_data: HostData){
-
-
+    pub fn add_to_cache_data(&mut self, rtype: Rtype, domain_name: String, rr_cache:RRCache){
+        let mut cache_data = self.get_cache_data();
+        rr_type_str = Rtype::from_rtype_to_str(rtype);
+        if let Some(x) = cache_data.get_mut(&rr_type_str) { 
+            let mut type_hash: HostData = x.clone();
+            type_hash.add_to_host_data(domain_name, rr_cache);
+            cache_data.insert(rtype, type_hash);
+        }
+        else {
+            let mut type_hash: HostData = HostData::new();
+            type_hash.add_to_host_data(domain_name, rr_cache);
+            cache_data.insert(rtype, type_hash);
+        }
+        self.set_cache_data(cache_data);
     }
 
     pub fn remove_from_cache_data(&mut self, domain_name: String, rtype: Rtype){

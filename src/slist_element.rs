bae93a6808ffa7f1b54e1d2e19eddf0dd9bfe9d9
Litr0 <gonzalo.sobarzoa@gmail.com>
2023-07-28 17:33:27 -0400

    slist element fixed

--- src/slist_element.rs
+++ src/slist_element.rs
@@ -1,5 +1,5 @@
 use crate::domain_name::DomainName;
-use std::net::{SocketAddr,IpAddr,Ipv4Addr};
+use std::net::IpAddr;
 /// This struct is used to represente an element of the slist
 #[derive(Debug)]
 pub struct SlistElement{
@@ -8,7 +8,7 @@ pub struct SlistElement{
     response_time: u16,
 }
 
-impl SlistElemen{
+impl SlistElement{
     /// function to create a new SlistElement
     /// # Example
     /// ```
@@ -21,7 +21,7 @@ impl SlistElemen{
     /// * `response_time` - A u16 that represents the response time of the slist element
     pub fn new(domain_name: DomainName, ip_address: IpAddr, response_time: u16) -> SlistElement{
         SlistElement{
-            domain_name: DomainName,
+            domain_name: domain_name,
             ip_address: ip_address,
             response_time: response_time,
         }
@@ -43,7 +43,7 @@ impl SlistElement{
     }
 
     pub fn set_domain_name(&mut self, domain_name: DomainName){
-        self.domain_name = DomainName
+        self.domain_name = domain_name
     }
 
     pub fn set_response_time(&mut self, response_time: u16){
@@ -57,12 +57,12 @@ impl SlistElement{
 
 #[cfg(test)]
 mod slist_element_test{
-    use std::net::{SocketAddr,IpAddr,Ipv4Addr};
-
+    use std::net::{IpAddr,Ipv4Addr};
+    use crate::{domain_name::DomainName, slist_element::SlistElement};
     #[test]
     //Constructor Test
     fn new_slist_element(){
-        let domain_name = DomainName::new();
+        let mut domain_name = DomainName::new();
         domain_name.set_name(String::from("uchile.cl"));
         let slist_element = SlistElement::new(domain_name, IpAddr::V4(Ipv4Addr::new(127, 0, 0, 1)), 4);
 
@@ -74,7 +74,7 @@ mod slist_element_test{
     #[test]
     //Getters and Setters Test
     fn get_address(){
-        let domain_name = DomainName::new();
+        let mut domain_name = DomainName::new();
         domain_name.set_name(String::from("uchile.cl"));
         let slist_element = SlistElement::new(domain_name, IpAddr::V4(Ipv4Addr::new(127, 0, 0, 1)), 4);
 
@@ -85,11 +85,11 @@ mod slist_element_test{
 
     #[test]
     fn set_ip_address(){
-        let domain_name = DomainName::new();
+        let mut domain_name = DomainName::new();
         domain_name.set_name(String::from("uchile.cl"));
-        let slist_element = SlistElement::new(domain_name, IpAddr::V4(Ipv4Addr::new(127, 0, 0, 1)), 4);
+        let mut slist_element = SlistElement::new(domain_name, IpAddr::V4(Ipv4Addr::new(127, 0, 0, 1)), 4);
 
-        assert_eq!(Ok(slist_element.get_address()), "127.0.0.1".parse());
+        assert_eq!(Ok(slist_element.get_ip_address()), "127.0.0.1".parse());
 
         slist_element.set_ip_address(IpAddr::V4(Ipv4Addr::new(192, 168, 0, 1)));
 
@@ -98,7 +98,7 @@ mod slist_element_test{
 
     #[test]
     fn get_response_time(){
-        let domain_name = DomainName::new();
+        let mut domain_name = DomainName::new();
         domain_name.set_name(String::from("uchile.cl"));
         let slist_element = SlistElement::new(domain_name, IpAddr::V4(Ipv4Addr::new(127, 0, 0, 1)), 4);
 
@@ -107,9 +107,9 @@ mod slist_element_test{
 
     #[test]
     fn set_response_time(){
-        let domain_name = DomainName::new();
+        let mut domain_name = DomainName::new();
         domain_name.set_name(String::from("uchile.cl"));
-        let slist_element = SlistElement::new(domain_name, IpAddr::V4(Ipv4Addr::new(127, 0, 0, 1)), 4);
+        let mut slist_element = SlistElement::new(domain_name, IpAddr::V4(Ipv4Addr::new(127, 0, 0, 1)), 4);
 
         assert_eq!(slist_element.get_response_time(), 4);
 
@@ -120,7 +120,7 @@ mod slist_element_test{
 
     #[test]
     fn get_domain_name(){
-        let domain_name = DomainName::new();
+        let mut domain_name = DomainName::new();
         domain_name.set_name(String::from("uchile.cl"));
         let slist_element = SlistElement::new(domain_name, IpAddr::V4(Ipv4Addr::new(127, 0, 0, 1)), 4);
 
@@ -129,13 +129,13 @@ mod slist_element_test{
 
     #[test]
     fn set_domain_name(){
-        let domain_name = DomainName::new();
+        let mut domain_name = DomainName::new();
         domain_name.set_name(String::from("uchile.cl"));
-        let slist_element = SlistElement::new(domain_name, IpAddr::V4(Ipv4Addr::new(127, 0, 0, 1)), 4);
+        let mut slist_element = SlistElement::new(domain_name, IpAddr::V4(Ipv4Addr::new(127, 0, 0, 1)), 4);
 
         assert_eq!(slist_element.get_domain_name().get_name(), String::from("uchile.cl"));
 
-        let domain_name_2 = DomainName::new();
+        let mut domain_name_2 = DomainName::new();
         domain_name_2.set_name(String::from("google.com"));
         slist_element.set_domain_name(domain_name_2);
 

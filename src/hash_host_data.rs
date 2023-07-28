b5f3e6b9afb6472f11e42ce4375d6bee53c31a2f
Litr0 <gonzalo.sobarzoa@gmail.com>
2023-07-28 14:53:45 -0400

    add and remove from host data test

--- src/hash_host_data.rs
+++ src/hash_host_data.rs
@@ -56,8 +56,10 @@ impl HostData{
     /// # Example
     /// ```
     /// let mut host_data = HostData::new();
-    /// let rr_cache = RRCache::new();
-    /// let domain_name = DomainName::new();
+    /// let a_rdata = Rdata::SomeARdata(ARdata::new());
+    /// let resource_record = ResourceRecord::new(a_rdata);
+    /// let rr_cache = RRCache::new(resource_record);
+    /// let mut domain_name = DomainName::new();
     /// domain_name.set_name(String::from("uchile.cl"));
     /// host_data.add_to_host_data(domain_name, rr_cache);
     /// host_data.remove_from_host_data(domain_name);
@@ -84,11 +86,13 @@ impl HostData{
 
 #[cfg(test)]
 mod host_data_test{
+    use crate::message::rdata::txt_rdata::TxtRdata;
     use crate::rr_cache::RRCache;
     use crate::domain_name::DomainName;
     use crate::message::rdata::Rdata;
     use crate::message::rdata::a_rdata::ARdata;
     use crate::message::resource_record::ResourceRecord;
+    use std::arch::x86_64::_CMP_NEQ_OS;
     use std::collections::HashMap;
 
     use super::HostData;
@@ -109,10 +113,43 @@ mod host_data_test{
         let rr_cache = RRCache::new(resource_record);
         let mut domain_name = DomainName::new();
         domain_name.set_name(String::from("uchile.cl"));
-        host_data.add_to_host_data(domain_name, rr_cache);
+        host_data.add_to_host_data(domain_name.clone(), rr_cache);
 
         let host_hash = host_data.get_host_hash();
 
         assert_eq!(host_hash.len(), 1);
+
+        let mut new_vec = Vec::new();
+        new_vec.push(String::from("hola"));
+        let text_rdata = Rdata::SomeTxtRdata(TxtRdata::new(new_vec));
+        let resource_record_2 = ResourceRecord::new(text_rdata);
+        let rr_cache_2 = RRCache::new(resource_record_2);
+        host_data.add_to_host_data(domain_name.clone(), rr_cache_2);
+
+        assert_eq!(host_hash.len(), 1);
+
+        let host_hash_vec = host_hash.get(&domain_name).unwrap();
+
+        assert_eq!(host_hash_vec.len(), 2);
+    }
+
+    //remove_from_host_data test
+    #[test]
+    fn remove_from_host_data(){
+        let mut host_data = HostData::new();
+        let a_rdata = Rdata::SomeARdata(ARdata::new());
+        let resource_record = ResourceRecord::new(a_rdata);
+        let rr_cache = RRCache::new(resource_record);
+        let mut domain_name = DomainName::new();
+        domain_name.set_name(String::from("uchile.cl"));
+        host_data.add_to_host_data(domain_name.clone(), rr_cache);
+
+        let host_hash = host_data.get_host_hash();
+
+        assert_eq!(host_hash.len(), 1);
+
+        host_data.remove_from_host_data(domain_name.clone());
+
+        assert_eq!(host_hash.len(), 0);
     }
 }

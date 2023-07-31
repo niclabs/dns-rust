63651b32ec0833181e43674d494beac5ad9c71fd
Litr0 <gonzalo.sobarzoa@gmail.com>
2023-07-31 15:27:23 -0400

    now rtype can be used as a hash key

--- src/message/type_rtype.rs
+++ src/message/type_rtype.rs
@@ -1,4 +1,4 @@
-#[derive(Clone, PartialEq, Debug, Eq,Hash)]
+#[derive(Clone, PartialEq, Debug, Hash, PartialOrd, Ord, Eq)]
 /// Enum For the Type of a RR in a DnsMessage with an Rdata implementation
 pub enum Rtype {
     A,

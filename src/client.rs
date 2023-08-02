00918f1f68fa33df3c30969a949471bbc87d0e77
Litr0 <gonzalo.sobarzoa@gmail.com>
2023-08-02 13:13:52 -0400

    wrong written domain udp

--- src/client.rs
+++ src/client.rs
@@ -362,15 +362,31 @@ mod client_test {
         
     }
 
-    //Wrong domain that haves a number at the start
+    //Wrong domain that haves a number at the start tcp
     #[test]
     #[should_panic]
-    fn wrong_written_domain_2(){
+    fn wrong_written_domain_2_tcp(){
         let server_addr:IpAddr = IpAddr::V4(Ipv4Addr::new(8, 8, 8, 8));
         let timeout: Duration = Duration::from_secs(2);
 
         let conn_tcp:ClientTCPConnection = ClientConnection::new(server_addr,timeout);
         let mut new_client = Client::new(conn_tcp);
+        let mut domain_name = DomainName::new();
+        domain_name.set_name(String::from("2www.u-cursos.cl"));
+        let mut response = new_client.query(domain_name, "A", "IN");
+
+        response.print_dns_message();
+    }
+
+    //Wrong domain that haves a number at the start udp
+    #[test]
+    #[should_panic]
+    fn wrong_written_domain_2_udp(){
+        let server_addr:IpAddr = IpAddr::V4(Ipv4Addr::new(8, 8, 8, 8));
+        let timeout: Duration = Duration::from_secs(2);
+
+        let conn_udp:ClientUDPConnection = ClientConnection::new(server_addr,timeout);
+        let mut new_client = Client::new(conn_udp);
         let mut domain_name = DomainName::new();
         domain_name.set_name(String::from("2www.u-cursos.cl"));
         let mut response = new_client.query(domain_name, "A", "IN");

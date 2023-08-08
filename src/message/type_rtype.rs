162865169d2ba0acc5acab7634636c48e7acb370
FranciscaOrtegaG <franciscaortega713@gmail.com>
2023-08-08 11:56:53 -0400

    Added copy  trait to rtype

--- src/message/type_rtype.rs
+++ src/message/type_rtype.rs
@@ -1,4 +1,4 @@
-#[derive(Clone, PartialEq, Debug, Hash, PartialOrd, Ord, Eq)]
+#[derive(Clone, PartialEq, Debug, Hash, PartialOrd, Ord, Eq, Copy)]
 /// Enum For the Type of a RR in a DnsMessage with an Rdata implementation
 pub enum Rtype {
     A,

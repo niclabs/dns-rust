724656f55e034f23b84a7c712de19fd948950001
Litr0 <gonzalo.sobarzoa@gmail.com>
2023-08-14 13:43:04 -0400

    get from dns cache test

--- src/dns_cache.rs
+++ src/dns_cache.rs
@@ -380,7 +380,38 @@ mod dns_cache_test {
         assert_eq!(cache.get_cache().get_cache_data().len(), 2);
         assert_eq!(cache.get_size(), 1);
     }
-    //Remaining test: Remove, get, remove_oldest_used, get_response_time, update_response_time
+
+    //Get test
+    #[test]
+    fn get_from_dns_cache(){
+        let mut cache = DnsCache::new();
+
+        cache.set_max_size(2);
+
+        let mut domain_name = DomainName::new();
+        domain_name.set_name(String::from("uchile.cl"));
+        let a_rdata = Rdata::SomeARdata(ARdata::new());
+        let resource_record = ResourceRecord::new(a_rdata);
+
+        cache.add(domain_name.clone(), resource_record);
+
+        assert_eq!(cache.get_cache().get_cache_data().len(), 1);
+
+        let mut new_vec = Vec::new();
+        new_vec.push(String::from("hola"));
+        let text_rdata = Rdata::SomeTxtRdata(TxtRdata::new(new_vec));
+        let resource_record_2 = ResourceRecord::new(text_rdata);
+
+        cache.add(domain_name.clone(), resource_record_2);
+
+        assert_eq!(cache.get_cache().get_cache_data().len(), 2);
+        assert_eq!(cache.get_size(), 2);
+
+        let cache_element = cache.get(domain_name.clone(), Rtype::TXT).unwrap();
+
+        assert_eq!(cache_element.len(), 1);
+    }
+    //Remaining test: get, remove_oldest_used, get_response_time, update_response_time
 //     #[test]
 //     fn update_and_get_response_time() {
 //         let mut dns_cache = DnsCache::new();

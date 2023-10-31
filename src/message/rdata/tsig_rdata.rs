69dac331d108a7a10c0ad1f73ed21af8e621b42c
Litr0 <gonzalo.sobarzoa@gmail.com>
2023-10-31 17:34:21 -0300

    implementation of getters for TSIG rdata

--- src/message/rdata/tsig_rdata.rs
+++ src/message/rdata/tsig_rdata.rs
@@ -41,3 +41,52 @@ impl TSigRdata {
         }
     }
 }
+
+/// Getters
+impl TSigRdata {
+
+    /// Gets the algorithm_name attribute from TSigRdata
+    fn get_algorithm_name(&self) -> DomainName {
+        self.algorithm_name.clone()
+    }
+
+    /// Gets the time_signed attribute from TSigRdata
+    fn get_time_signed(&self) -> u64 {
+        self.time_signed
+    }
+
+    /// Gets the fudge attribute from TSigRdata
+    fn get_fudge(&self) -> u16 {
+        self.fudge
+    }
+    
+    /// Gets the mac_size attribute from TSigRdata
+    fn get_mac_size(&self) -> u16 {
+        self.mac_size
+    }
+
+    /// Gets the mac attribute from TSigRdata
+    fn get_mac(&self) -> Vec<u8> {
+        self.mac.clone()
+    }
+
+    /// Gets the original_id attribute from TSigRdata
+    fn get_original_id(&self) -> u16 {
+        self.original_id
+    }
+
+    /// Gets the error attribute from TSigRdata
+    fn get_error(&self) -> u16 {
+        self.error
+    }
+
+    /// Gets the other_len attribute from TSigRdata
+    fn get_other_len(&self) -> u16 {
+        self.other_len
+    }
+
+    /// Gets the other_data attribute from TSigRdata
+    fn get_other_data(&self) -> Vec<u8> {
+        self.other_data.clone()
+    }
+}

e9bd66a4f8d1fe3dd958c3fabacee929f4299003
Litr0 <gonzalo.sobarzoa@gmail.com>
2023-11-02 14:37:52 -0300

    set and get error test

--- src/message/rdata/tsig_rdata.rs
+++ src/message/rdata/tsig_rdata.rs
@@ -475,4 +475,15 @@ mod tsig_rdata_test {
 
         assert_eq!(tsig_rdata.get_original_id(), 1234);
     }
+
+    #[test]
+    fn set_and_get_error(){
+        let mut tsig_rdata = TSigRdata::new();
+
+        assert_eq!(tsig_rdata.get_error(), 0);
+
+        tsig_rdata.set_error(1234);
+
+        assert_eq!(tsig_rdata.get_error(), 1234);
+    }
 }

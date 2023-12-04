dd4c117ab5797beb0d51b0041e7e9778e10d9d45
Ephy <vicente.ardiles@ug.uchile.cl>
2023-12-04 02:15:01 -0300

    add ns query integration tests

--- tests/integration_test.rs
+++ tests/integration_test.rs
@@ -1,6 +1,6 @@
 use std::{net::IpAddr, str::FromStr};
 
-use dns_rust::{async_resolver::{config::ResolverConfig, AsyncResolver, resolver_error::ResolverError}, message::{resource_record::ResourceRecord, rdata::Rdata}};
+use dns_rust::{async_resolver::{config::ResolverConfig, AsyncResolver, resolver_error::ResolverError}, message::{resource_record::ResourceRecord, rdata::Rdata}, domain_name::DomainName};
 
 
 
@@ -30,7 +30,7 @@ async fn query_a_type() {
         if let Rdata::A(ip) = rdata {
             assert_eq!(ip.get_address(), IpAddr::from_str("93.184.216.34").unwrap());
         } else {
-            panic!("Error parsing response");
+            panic!("No ip address");
         }
     } else {
         panic!("No response")
@@ -42,7 +42,7 @@ async fn query_a_type() {
 async fn query_all_type() {
     let response = query_response("example.com", "ANY").await;
     if let Ok(rrs) = response {
-        assert_eq!(rrs.iter().count(), 2);
+        assert_eq!(rrs.len(), 2);
     } else {
         panic!("No response")
     }
@@ -50,7 +50,34 @@ async fn query_all_type() {
 
 // TODO: 6.2.3 Query normal Qtype = MX
 
+
 // TODO: 6.2.4 Query normal Qtype = NS
+#[tokio::test]
+async fn query_ns_type() {
+    let response = query_response("example.com", "NS").await;
+    if let Ok(rrs) = response {
+        assert_eq!(rrs.len(), 2);
+        
+        if let Rdata::NS(ns1) = rrs[0].get_rdata() {
+            assert_eq!(
+                ns1.get_nsdname(),
+                DomainName::new_from_str("a.iana-servers.net"))
+        } else { 
+            panic!("First record is not type NS");
+        }
+        
+        if let Rdata::NS(ns) = rrs[1].get_rdata() {
+            assert_eq!(
+                ns.get_nsdname(),
+                DomainName::new_from_str("b.iana-servers.net"))
+        } else {
+            panic!("Second record is not type NS");
+        }
+
+    } else {
+        panic!("No response received")
+    }
+}
 
 // TODO: 6.2.5 Dominio mal escrito Qtype = A
 

97c00473c3594013d83dafa67b39115ad898c602
Litr0 <gonzalo.sobarzoa@gmail.com>
2023-12-13 12:18:57 -0300

    Refactor OptRdata struct and implement ToBytes trait
    This commit refactors the OptRdata struct by separating the implementation of the ToBytes trait into its own block. It also adds constructor, getter, and setter methods for OptRdata.

--- src/message/rdata/opt_rdata.rs
+++ src/message/rdata/opt_rdata.rs
@@ -23,3 +23,54 @@ pub struct OptRdata {
     pub option_length: u16,
     pub option_data: Vec<u8>,
 }
+
+impl ToBytes for OptRdata {
+    /// Returns a `Vec<u8>` of bytes that represents the OPT RDATA.
+    fn to_bytes(&self) -> Vec<u8> {
+        let mut bytes: Vec<u8> = Vec::new();
+
+        bytes.extend_from_slice(&self.option_code.to_be_bytes());
+        bytes.extend_from_slice(&self.option_length.to_be_bytes());
+        bytes.extend_from_slice(&self.option_data);
+
+        bytes
+    }
+}
+
+/// Constructor and getters for OptRdata
+impl OptRdata {
+    pub fn new() -> Self {
+        OptRdata {
+            option_code: 0,
+            option_length: 0,
+            option_data: Vec::new(),
+        }
+    }
+
+    pub fn get_option_code(&self) -> u16 {
+        self.option_code.clone()
+    }
+
+    pub fn get_option_length(&self) -> u16 {
+        self.option_length.clone()
+    }
+
+    pub fn get_option_data(&self) -> Vec<u8> {
+        self.option_data.clone()
+    }
+}
+
+/// Setters for OptRdata
+impl OptRdata {
+    pub fn set_option_code(&mut self, option_code: u16) {
+        self.option_code = option_code;
+    }
+
+    pub fn set_option_length(&mut self, option_length: u16) {
+        self.option_length = option_length;
+    }
+
+    pub fn set_option_data(&mut self, option_data: Vec<u8>) {
+        self.option_data = option_data;
+    }
+}

1a32fe0c1d198006f5ab5a76cdff26c1ef2c9bc4
Litr0 <gonzalo.sobarzoa@gmail.com>
2023-12-19 11:11:55 -0300

    Add DnskeyRdata struct for DNSKEY Rdata

--- src/message/rdata/dnskey_rdata.rs
+++ src/message/rdata/dnskey_rdata.rs
@@ -1,1 +1,22 @@
 use crate::message::resource_record::{FromBytes, ToBytes};
+
+#[derive(Clone, Debug, PartialEq)]
+/// Struct for DNSKEY Rdata
+///                       1 1 1 1 1 1 1 1 1 1 2 2 2 2 2 2 2 2 2 2 3 3
+/// 0 1 2 3 4 5 6 7 8 9 0 1 2 3 4 5 6 7 8 9 0 1 2 3 4 5 6 7 8 9 0 1
+/// +-+-+-+-+-+-+-+-+-+-+-+-+-+-+-+-+-+-+-+-+-+-+-+-+-+-+-+-+-+-+-+-+
+/// |              Flags            |    Protocol   |   Algorithm   |
+/// +-+-+-+-+-+-+-+-+-+-+-+-+-+-+-+-+-+-+-+-+-+-+-+-+-+-+-+-+-+-+-+-+
+/// /                                                               /
+/// /                            Public Key                         /
+/// /                                                               /
+/// +-+-+-+-+-+-+-+-+-+-+-+-+-+-+-+-+-+-+-+-+-+-+-+-+-+-+-+-+-+-+-+-+
+/// 
+/// [RFC 4034](https://tools.ietf.org/html/rfc4034#section-2.1.)
+
+pub struct DnskeyRdata {
+    pub flags: u16,
+    pub protocol: u8,
+    pub algorithm: u8,
+    pub public_key: Vec<u8>,
+}

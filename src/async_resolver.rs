6b69250e7bde0d9cdf360872ee24b3a67f09f86b
NegroDCC <bastian.corrales@ug.uchile.cl>
2023-12-21 16:51:18 -0300

    add test qtype SOA inner_lookup in async_resolver

--- src/async_resolver.rs
+++ src/async_resolver.rs
@@ -530,6 +530,27 @@ mod async_resolver_test {
     }
 
     #[tokio::test]
+    async fn inner_lookup_qtype_soa() {
+        // Create a new resolver with default values
+        let mut resolver = AsyncResolver::new(ResolverConfig::default());
+        let domain_name = DomainName::new_from_string("example.com".to_string());
+        let qtype = Qtype::SOA;
+        let record_class = Qclass::IN;
+        let response = resolver.inner_lookup(domain_name,qtype,record_class).await;
+
+        let response = match response {
+            Ok(val) => val,
+            Err(error) => panic!("Error in the response: {:?}", error),
+        };
+        //analize if the response has the correct type according with the qtype
+        let answers = response.get_answer();
+        for answer in answers {
+            let soa_rdata = answer.get_rdata();
+            // Check if the answer is SOA type
+            assert!(matches!(soa_rdata, Rdata::SOA(_soa_rdata)))
+        }
+    }
+    #[tokio::test]
     async fn inner_lookup_ns() {
         // Create a new resolver with default values
         let mut resolver = AsyncResolver::new(ResolverConfig::default());

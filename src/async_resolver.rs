6ed6f0b4af8bba78ec87626aeec5360abb94db8d
NegroDCC <bastian.corrales@ug.uchile.cl>
2023-12-21 16:35:46 -0300

    add test qtype A inner_lookup in async_resolver

--- src/async_resolver.rs
+++ src/async_resolver.rs
@@ -441,7 +441,7 @@ mod async_resolver_test {
     }
 
     #[tokio::test]
-    async fn inner_lookup() {
+    async fn inner_lookup_qtype_a() {
         // Create a new resolver with default values
         let mut resolver = AsyncResolver::new(ResolverConfig::default());
         let domain_name = DomainName::new_from_string("example.com".to_string());
@@ -449,10 +449,19 @@ mod async_resolver_test {
         let record_class = Qclass::IN;
         let response = resolver.inner_lookup(domain_name,qtype,record_class).await;
 
-        //FIXME: add assert
-        assert!(response.is_ok());
-    } 
-
+        let response = match response {
+            Ok(val) => val,
+            Err(error) => panic!("Error in the response: {:?}", error),
+        };
+        //analize if the response has the correct type according with the qtype
+        let answers = response.get_answer();
+        for answer in answers {
+            let a_rdata = answer.get_rdata();
+            // Check if the answer is A type
+            assert!(matches!(a_rdata, Rdata::A(_a_rdata)))
+        }
+    }
+ 
     #[tokio::test]
     async fn inner_lookup_ns() {
         // Create a new resolver with default values

50a0258cb8bfb234469bcf25dbee73b0f846b479
NegroDCC <bastian.corrales@ug.uchile.cl>
2023-12-21 16:48:30 -0300

    add test qtype MX inner_lookup in async_resolver

--- src/async_resolver.rs
+++ src/async_resolver.rs
@@ -484,7 +484,29 @@ mod async_resolver_test {
             assert!(matches!(ns_rdata, Rdata::NS(_ns_rdata)))
         }
     }
- 
+
+    #[tokio::test]
+    async fn inner_lookup_qtype_mx() {
+        // Create a new resolver with default values
+        let mut resolver = AsyncResolver::new(ResolverConfig::default());
+        let domain_name = DomainName::new_from_string("example.com".to_string());
+        let qtype = Qtype::MX;
+        let record_class = Qclass::IN;
+        let response = resolver.inner_lookup(domain_name,qtype,record_class).await;
+
+        let response = match response {
+            Ok(val) => val,
+            Err(error) => panic!("Error in the response: {:?}", error),
+        };
+        //analize if the response has the correct type according with the qtype
+        let answers = response.get_answer();
+        for answer in answers {
+            let mx_rdata = answer.get_rdata();
+            // Check if the answer is MX type
+            assert!(matches!(mx_rdata, Rdata::MX(_mx_rdata)))
+        }
+    }
+
     #[tokio::test]
     async fn inner_lookup_ns() {
         // Create a new resolver with default values

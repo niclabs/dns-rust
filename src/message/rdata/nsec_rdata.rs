bff1aeb4a1be7fb9b31c220fdce2a0eeb43b0288
NegroDCC <bastian.corrales@ug.uchile.cl>
2024-01-16 14:57:49 -0300

    add test max value for unknown from bytes in nsec

--- src/message/rdata/nsec_rdata.rs
+++ src/message/rdata/nsec_rdata.rs
@@ -319,4 +319,33 @@ mod nsec_rdata_test{
 
         assert_eq!(nsec_rdata.to_bytes(), bytes_to_test);
     }
+
+    #[test]
+    fn from_bytes_max_value_unknown(){
+        let next_domain_name_bytes = vec![4, 104, 111, 115, 116, 7, 101, 120, 97, 109, 112, 108, 101, 3, 99, 111, 109, 0];
+
+        //this shoud represent the 65535 value, because the windown number is 255 -> start in
+        // 255*Number_bytes_for_windon*8 - 1 = 255*32*8 - 1 = 65279, need 256: 32*8 = 256, so 
+        // 31 zeros and 00000001 = 1 
+        let bit_map_bytes_to_test = vec![255, 32,
+        0, 0, 0, 0, 0, 0, 0, 0, // 8
+        0, 0, 0, 0, 0, 0, 0, 0, //16
+        0, 0, 0, 0, 0, 0, 0, 0, //24
+        0, 0, 0, 0, 0, 0, 0, 1]; //32
+
+        let bytes_to_test = [next_domain_name_bytes, bit_map_bytes_to_test].concat();
+
+        let nsec_rdata = NsecRdata::from_bytes(&bytes_to_test, &bytes_to_test).unwrap();
+
+        let expected_next_domain_name = String::from("host.example.com");
+
+        assert_eq!(nsec_rdata.get_next_domain_name().get_name(), expected_next_domain_name);
+
+        let expected_type_bit_maps = vec![Rtype::UNKNOWN(65535)];
+
+        assert_eq!(nsec_rdata.get_type_bit_maps(), expected_type_bit_maps);
+
+    }
+
+    
 }

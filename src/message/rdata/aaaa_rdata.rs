2f04408e9a65ba51839d77d94622dfe16802a78b
Litr0 <gonzalo.sobarzoa@gmail.com>
2024-03-06 12:39:32 -0300

    Add AAAA Rdata struct for IPv6 addresses

---
+++ src/message/rdata/aaaa_rdata.rs
@@ -0,0 +1,20 @@
+use crate::domain_name::DomainName;
+use crate::message::rdata::Rdata;
+use crate::message::Rtype;
+use crate::message::Rclass;
+use crate::message::resource_record::{FromBytes, ResourceRecord, ToBytes};
+use std::net::IpAddr;
+
+use std::str::SplitWhitespace;
+
+/// Struct for the AAAA Rdata
+/// 2.2 AAAA data format
+///
+/// A 128 bit IPv6 address is encoded in the data portion of an AAAA
+/// resource record in network byte order (high-order byte first).
+
+#[derive(Clone, PartialEq, Debug)]
+pub struct AAAARdata {
+    /// A 128 bit Internet address.
+    address: IpAddr,
+}

98de87e3e68d6dcf0fb668a1985f71194b1f48ae
Katia Fredes <katia.fredes@ug.uchile.cl>
2024-03-25 13:25:45 -0300

    add correct docs to LookupResponse

--- src/async_resolver/lookup_response.rs
+++ src/async_resolver/lookup_response.rs
@@ -2,20 +2,26 @@ use crate::message::{resource_record::ResourceRecord, DnsMessage};
 
 /// This struct represents the response of a DNS lookup.
 /// 
-/// It contains the list of IP addresses associated with the domain name. Depending
-/// on the resquested format of the response, the IP addresses can be represented
-/// as strings, structs or bytes.
+/// This response corresponds to the return value of the `lookup` method of 
+/// the `AsyncResolver` struct. It is a wrapper around a `DnsMessage` struct.
+/// With the methods of this struct, you can convert the response to a string,
+/// a byte vector, a `DnsMessage` struct or a vector of `ResourceRecord` structs,
+/// depending on your needs.
 #[derive(Clone, Debug)]
 pub struct LookupResponse {
+    // The DNS message response.
     dns_msg_response: DnsMessage,
 }
 
 impl LookupResponse {
-    /// Create a new LookupResponse instance.
+    /// Create a new `LookupResponse` instance.
     pub fn new(dns_msg_response: DnsMessage) -> LookupResponse {
         LookupResponse { dns_msg_response }
     }
 
+
+    // FIXME: make this method an implementation of the Display trait.
+    /// Convert the response to a string.
     pub fn to_string(&self) -> String {
         let mut result = String::new();
         for address in &self.dns_msg_response.get_answer() {
@@ -24,20 +30,31 @@ impl LookupResponse {
         result
     }
 
+    // FIXME: make this method an implementation of to_bytes trait.
+    /// Convert the response to a byte vector.
     pub fn to_bytes(&self) -> Vec<u8> {
         self.dns_msg_response.to_bytes()
     }
 
+    /// Convert the response to a `DnsMessage` struct.
+    /// 
+    /// This method returns a clone of the `DnsMessage` struct that is 
+    /// wrapped. This is useful if you want to access the `DnsMessage`
+    /// using this library's API.
     pub fn to_dns_msg(&self) -> DnsMessage {
         self.dns_msg_response.clone()
     }
 
+    /// Convert the response to a vector of `ResourceRecord` structs.
+    /// 
+    /// This method returns a vector of `ResourceRecord` structs that are
+    /// contained in the `DnsMessage` struct that is wrapped. This is useful
+    /// if you want to access the `ResourceRecord` structs using this library's
+    /// API.
     pub fn to_vec_of_rr(&self) -> Vec<ResourceRecord> {
         self.dns_msg_response.get_answer()
     }
 }
-
-
 
 #[cfg(test)]
 mod lookup_response_tests {

064b95b999ebfc9b7fb9adadbfa4c7fad185297e
Litr0 <gonzalo.sobarzoa@gmail.com>
2024-04-26 13:12:46 -0400

    Add test for add function in DnsCache module

--- src/dns_cache.rs
+++ src/dns_cache.rs
@@ -59,6 +59,7 @@ impl DnsCache {
         // see cache space
     }
 
+    /// TODO: Crear test y mejorar función de acuerdo a RFC de Negative caching
     /// Add negative resource record type SOA to cache for negative answers
     pub fn add_negative_answer(&mut self, domain_name: DomainName, rtype: Rtype, resource_record:ResourceRecord) {
         let mut cache_data = self.get_cache();
@@ -229,6 +230,7 @@ impl DnsCache {
 #[cfg(test)] 
 mod dns_cache_test {
     use super::*;
+    use crate::message::rdata::a_rdata::ARdata;
 
     #[test]
     fn test_new() {
@@ -274,5 +276,30 @@ mod dns_cache_test {
         cache.set_max_size(max_size.clone());
 
         assert_eq!(cache.get_max_size(), max_size);
+    }
+
+    #[test]
+    fn add() {
+        let mut cache = DnsCache::new(NonZeroUsize::new(10));
+        let domain_name = DomainName::new_from_str("example.com");
+        let ip_address = IpAddr::from([127, 0, 0, 0]);
+        let mut a_rdata = ARdata::new();
+        a_rdata.set_address(ip_address);
+        let rdata = Rdata::A(a_rdata);
+        let mut resource_record = ResourceRecord::new(rdata);
+        resource_record.set_name(domain_name.clone());
+        resource_record.set_type_code(Rtype::A);
+
+        cache.add(domain_name.clone(), resource_record.clone());
+
+        let rr_cache_vec = cache.get(domain_name.clone(), Rtype::A).unwrap();
+
+        let first_rr_cache = rr_cache_vec.first().unwrap();
+
+        assert_eq!(rr_cache_vec.len(), 1);
+
+        assert_eq!(first_rr_cache.get_resource_record().get_rtype(), Rtype::A);
+
+        assert_eq!(first_rr_cache.get_resource_record().get_name(), domain_name.clone());
     }
 }

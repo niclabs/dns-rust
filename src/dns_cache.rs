bb2d1ca59ee2db45c10b0080802e7ae972e03fc9
Litr0 <gonzalo.sobarzoa@gmail.com>
2024-04-26 13:18:01 -0400

    Add test for add_two_elements_same_type_and_domain_name function in DnsCache module

--- src/dns_cache.rs
+++ src/dns_cache.rs
@@ -302,4 +302,33 @@ mod dns_cache_test {
 
         assert_eq!(first_rr_cache.get_resource_record().get_name(), domain_name.clone());
     }
+
+    #[test]
+    fn add_two_elements_same_type_and_domain_name(){
+        let mut cache = DnsCache::new(NonZeroUsize::new(10));
+        let domain_name = DomainName::new_from_str("example.com");
+        let ip_address = IpAddr::from([127, 0, 0, 0]);
+        let mut a_rdata = ARdata::new();
+        a_rdata.set_address(ip_address);
+        let rdata = Rdata::A(a_rdata);
+        let mut resource_record = ResourceRecord::new(rdata);
+        resource_record.set_name(domain_name.clone());
+        resource_record.set_type_code(Rtype::A);
+
+        cache.add(domain_name.clone(), resource_record.clone());
+
+        let ip_address = IpAddr::from([127, 0, 0, 1]);
+        let mut a_rdata = ARdata::new();
+        a_rdata.set_address(ip_address);
+        let rdata = Rdata::A(a_rdata);
+        let mut resource_record = ResourceRecord::new(rdata);
+        resource_record.set_name(domain_name.clone());
+        resource_record.set_type_code(Rtype::A);
+
+        cache.add(domain_name.clone(), resource_record.clone());
+
+        let rr_cache_vec = cache.get(domain_name.clone(), Rtype::A).unwrap();
+
+        assert_eq!(rr_cache_vec.len(), 2);
+    }
 }

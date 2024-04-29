32d0b18d6a9bf9505af79992be9ee54ae111371c
Litr0 <gonzalo.sobarzoa@gmail.com>
2024-04-29 13:49:28 -0400

    Test is_empty function in DnsCache module

--- src/dns_cache.rs
+++ src/dns_cache.rs
@@ -153,7 +153,7 @@ impl DnsCache {
                     Rdata::A(val) => val.get_address(),
                     _ => unreachable!(),
                 };
-                
+
                 if ip_address == rr_ip_address {
                     rr.set_response_time(response_time);
                 }
@@ -576,5 +576,25 @@ mod dns_cache_test {
         let response_time_obtained = cache.get_response_time(domain_name.clone(), Rtype::A, ip_address);
 
         assert_eq!(response_time_obtained, new_response_time);
+    }
+
+    #[test]
+    fn is_empty(){
+        let mut cache = DnsCache::new(NonZeroUsize::new(10));
+
+        assert!(cache.is_empty());
+
+        let domain_name = DomainName::new_from_str("example.com");
+        let ip_address = IpAddr::from([127, 0, 0, 0]);
+        let mut a_rdata = ARdata::new();
+        a_rdata.set_address(ip_address);
+        let rdata = Rdata::A(a_rdata); 
+        let mut resource_record = ResourceRecord::new(rdata);
+        resource_record.set_name(domain_name.clone());
+        resource_record.set_type_code(Rtype::A);
+        
+        cache.add(domain_name.clone(), resource_record.clone());
+
+        assert!(!cache.is_empty());
     }
 }

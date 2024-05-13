8d4068eb33f180e097668b048fbda40e449710c1
Carlo <carloarturomerinoc@gmail.com>
2024-05-13 14:22:18 -0400

    Fixed sign_msg  bugs with the hasher's data types

--- src/tsig.rs
+++ src/tsig.rs
@@ -5,19 +5,25 @@ use crate::message::type_qtype::Qtype;
 use crate::message::{rdata::tsig_rdata::TSigRdata, DnsMessage};
 use crate::message::rdata::Rdata;
 use sha2::Sha256;
-use sha1::Sha1;
+use crypto::hmac::Hmac as crypto_hmac;
+use crypto::mac::Mac as crypto_mac;
+use hmac::{Hmac, Mac};
+use crypto::sha1::Sha1;
+use bytes::Bytes;
+
+type HmacSha256 = Hmac<Sha256>;
 
 enum TsigAlgorithm {
     HmacSha1,
     HmacSha256,
 }
 
-/// This functions signs a DnsMessage with a key in bytes, and the 
-/// algName will be used to select the algorithm to encrypt the key.
-fn sign_msg(query_msg:DnsMessage,key:Bytes, alg_name:TsigAlgorithm)->Bytes{
-    let mut new_query_msg = query_msg.clone();
-    let mut dig_string = "";
-    let mut mac_len = "";
+#[doc = r"This functions signs a DnsMessage with a key in bytes, and the algName will be used to select the algorithm to encrypt the key."]
+fn sign_msg(query_msg:DnsMessage,key:&[u8], alg_name:TsigAlgorithm)->&[u8]{
+    let mut new_query_message = query_msg.clone();
+    let mut dig_string:&str;
+    let mut mac_len:&str;
+    let mut a_name: &str;
     let placeholder_hex: String; 
     match alg_name {
         TsigAlgorithm::HmacSha1 => {
@@ -29,26 +35,28 @@ fn sign_msg(query_msg:DnsMessage,key:Bytes, alg_name:TsigAlgorithm)->Bytes{
             placeholder_hex = placeholder.iter().map(|b| format!("{:02x}", b)).collect();
             dig_string = &placeholder_hex;
             mac_len = "20";
+            a_name = "Hmac-Sha1";
         },
         TsigAlgorithm::HmacSha256 => {
-            let mut hasher = HmacSha256::new_from_slice(key) .expect("HMAC algoritms can take keys of any size");
-            hasher.update(&dns_query_message.to_bytes()[..]);
+            let mut hasher = HmacSha256::new_from_slice(key).expect("HMAC algoritms can take keys of any size");
+            hasher.update(&new_query_message.to_bytes()[..]);
             let result = hasher.finalize();
 
             let code_bytes = result.into_bytes();
             placeholder_hex = hex::encode(code_bytes);
             dig_string = &placeholder_hex;
             mac_len = "32";
+            a_name = "Hmac-Sha256";
             
         },
-        _ => {panic!("Error")},
+        _ => {panic!("Error: Invalid algorithm")},
     }
     //TODO: agregar los demas valores al msg_bytes !! Yo creo que deben llegar como argumentos
-    msg_bytes = format!("{}.\n51921\n1234\n{}\n{}\n1234\n0\n0",alg_name, mac_len, dig_string);
-    return msg_bytes;
+    let mut msg_bytes: String = format!("{}.\n51921\n1234\n{}\n{}\n1234\n0\n0",a_name, mac_len, dig_string);
+    return msg_bytes.as_bytes();
 }
                                                             
-fn tsig_proccesing_answer(answer_msg:DnsMessage)->Bool{
+fn tsig_proccesing_answer(answer_msg:DnsMessage){
     //procesar los errores 
-    new_answer_msg = answer_msg.clone();
+    new_answer_msg = answer_msg.clone()
 }

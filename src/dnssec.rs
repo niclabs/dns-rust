8512bcb7292fadb05bff58ac1715d404fb6ade7e
joalopez <joalopez@ug.uchile.cl>
2024-05-24 16:15:26 -0400

    FIX: added requested udp len to OPT rr.

--- src/dnssec.rs
+++ src/dnssec.rs
@@ -4,6 +4,7 @@ pub mod dnssec_encryption;
 use std::str::FromStr;
 use crate::domain_name::DomainName;
 use crate::message::class_qclass::Qclass;
+use crate::message::class_rclass::Rclass;
 use crate::message::DnsMessage;
 use crate::message::rdata::opt_rdata::OptRdata;
 use crate::message::rdata::Rdata;
@@ -13,6 +14,7 @@ use crate::message::rcode;
 use crate::message::rcode::Rcode;
 
 const EDNS_VERSION: u8 = 0;
+const REQUESTED_UDP_LEN: u16 = 4096;
 /*
 The mechanism chosen for the explicit notification of the ability of
 the client to accept (if not understand) DNSSEC security RRs is using
@@ -29,8 +31,7 @@ of the EDNS0 OPT meta-RR, structured as follows:
   2: |DO|                    Z                       |
      +--+--+--+--+--+--+--+--+--+--+--+--+--+--+--+--+
 */
-// [0x12, 0x0, 0x80, 0x0]
-fn create_opt_rr(e_rcode :u8, version: u8, do_bit: bool) -> ResourceRecord {
+fn create_opt_rr(capacity: u16 ,e_rcode :u8, version: u8, do_bit: bool) -> ResourceRecord {
     let opt_rdata = OptRdata::new();
     let rdata = Rdata::OPT(opt_rdata);
     let mut rr = ResourceRecord::new(rdata);
@@ -38,17 +39,19 @@ fn create_opt_rr(e_rcode :u8, version: u8, do_bit: bool) -> ResourceRecord {
     let do_val: u16 = if do_bit {0x8000} else {0x0};
     let ttl: u32 = (e_rcode as u32) << 24 | (version as u32) << 16| (do_val as u32);
     rr.set_ttl(ttl);
+    rr.set_rclass(Rclass::UNKNOWN(capacity));
     println!("EL ttl es: {:#05x?}", ttl);
     rr
 }
 
 fn read_opt_rr(opt_rr: ResourceRecord) -> String {
+    let requested_udp_len = Rclass::from_rclass_to_int(opt_rr.get_rclass());
     let data = opt_rr.get_ttl().to_be_bytes();
     let (e_rcode, version) = (data[0], data[1]);
     let z = u16::from_be_bytes([data[2], data[3]]);
 
     let do_bit = ((z & 0x8000) > 0) as u8 ;
-    format!("OPT PSEUDO-RR\n\terror code: {e_rcode}\n\tversion: EDNS{version}\n\tuse dnssec: {do_bit}")
+    format!("OPT PSEUDO-RR\n\trequested_udp_len: {requested_udp_len}\n\terror code: {e_rcode}\n\tversion: EDNS{version}\n\tuse dnssec: {do_bit}")
 }
 
 /*
@@ -58,7 +61,7 @@ fn read_opt_rr(opt_rr: ResourceRecord) -> String {
 fn create_dns_message_with_dnssec(mut msg: DnsMessage) -> DnsMessage {
     // We create a opt rr with the do bit set to 1
     // with NOERR as rcode and EDNS0
-    let rr = create_opt_rr(
+    let rr = create_opt_rr(REQUESTED_UDP_LEN,
                             rcode::Rcode::from_rcode_to_int(Rcode::NOERROR),
                             EDNS_VERSION,
                             true);
@@ -73,14 +76,14 @@ fn see_dnssec_message() {
     let query = DnsMessage::new_query_message(
         DomainName::new_from_str("example.com"),
         Qtype::A,
-        Qclass::ANY,
+        Qclass::UNKNOWN(4096),
         1,
         true,
         2000
     );
     let query= create_dns_message_with_dnssec(query);
     assert_eq!(String::from_str
-                   ("OPT PSEUDO-RR\n\terror code: 0\n\tversion: EDNS0\n\tuse dnssec: 1")
+                   ("OPT PSEUDO-RR\n\trequested_udp_len: 4096\n\terror code: 0\n\tversion: EDNS0\n\tuse dnssec: 1")
                    .expect("Not a utf8 str"),
                read_opt_rr(query.get_additional().pop().expect("No OPT Record!"))
     )

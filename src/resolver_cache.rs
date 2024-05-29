0b04e31d2e25a5a9b691dfc36c4e19be4b0488c3
Litr0 <gonzalo.sobarzoa@gmail.com>
2024-05-29 15:40:05 -0400

    add constructor

--- src/resolver_cache.rs
+++ src/resolver_cache.rs
@@ -1,8 +1,21 @@
 use crate::dns_cache::DnsCache;
 
+use std::num::NonZeroUsize;
+
 #[derive(Clone, Debug)]
 pub struct ResolverCache {
     cache_answer: DnsCache,
     cache_authority: DnsCache,
     cache_additional: DnsCache,
+}
+
+impl ResolverCache {
+    pub fn new(size: Option<NonZeroUsize>) -> Self {
+        let size = size.unwrap_or(NonZeroUsize::new(1667).unwrap());
+        Self {
+            cache_answer: DnsCache::new(Some(size)),
+            cache_authority: DnsCache::new(Some(size)),
+            cache_additional: DnsCache::new(Some(size)),
+        }
+    }
 }

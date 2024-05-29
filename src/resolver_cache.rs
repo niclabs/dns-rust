bddbc71d419437bc07aff68c2d1f288f77b472f1
Litr0 <gonzalo.sobarzoa@gmail.com>
2024-05-29 15:46:20 -0400

    implementation getters and setters

--- src/resolver_cache.rs
+++ src/resolver_cache.rs
@@ -10,6 +10,8 @@ pub struct ResolverCache {
 }
 
 impl ResolverCache {
+
+    /// Create a new ResolverCache with the given size.
     pub fn new(size: Option<NonZeroUsize>) -> Self {
         let size = size.unwrap_or(NonZeroUsize::new(1667).unwrap());
         Self {
@@ -17,5 +19,42 @@ impl ResolverCache {
             cache_authority: DnsCache::new(Some(size)),
             cache_additional: DnsCache::new(Some(size)),
         }
+    }
+}
+
+impl ResolverCache {
+
+    /// Get the answer cache.
+    pub fn get_cache_answer(&self) -> &DnsCache {
+        &self.cache_answer
+    }
+
+    /// Get the authority cache.
+    pub fn get_cache_authority(&self) -> &DnsCache {
+        &self.cache_authority
+    }
+
+    /// Get the additional cache.
+    pub fn get_cache_additional(&self) -> &DnsCache {
+        &self.cache_additional
+    }
+}
+
+
+impl ResolverCache {
+
+    /// Set the answer cache.
+    pub fn set_cache_answer(&mut self, cache: DnsCache) {
+        self.cache_answer = cache;
+    }
+
+    /// Set the authority cache.
+    pub fn set_cache_authority(&mut self, cache: DnsCache) {
+        self.cache_authority = cache;
+    }
+
+    /// Set the additional cache.
+    pub fn set_cache_additional(&mut self, cache: DnsCache) {
+        self.cache_additional = cache;
     }
 }

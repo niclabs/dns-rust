53476931267f3e09a37ba4b3840ca41edfd0f982
Litr0 <gonzalo.sobarzoa@gmail.com>
2024-05-29 15:47:59 -0400

    Change default size

--- src/dns_cache.rs
+++ src/dns_cache.rs
@@ -35,7 +35,7 @@ impl DnsCache {
     ///
     pub fn new(max_size: Option<NonZeroUsize>) -> Self {
         let cache = DnsCache {
-            cache: LruCache::new(max_size.unwrap_or_else(|| NonZeroUsize::new(5000).unwrap())),
+            cache: LruCache::new(max_size.unwrap_or_else(|| NonZeroUsize::new(1667).unwrap())),
             max_size: max_size.unwrap_or_else(|| NonZeroUsize::new(100).unwrap()),
         };
         cache

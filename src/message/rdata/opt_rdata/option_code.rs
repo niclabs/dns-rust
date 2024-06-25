183a8b80525c36c6fea41d020efc33a7ae14837d
Litr0 <gonzalo.sobarzoa@gmail.com>
2024-06-25 13:11:19 -0400

    Refactor option_code submodule in opt rdata module

---
+++ src/message/rdata/opt_rdata/option_code.rs
@@ -0,0 +1,54 @@
+use std::fmt;
+
+/// Enum for the option code in an OPT Rdata
+pub enum OptionCode {
+    NSID,
+    PADDING,
+    UNKNOWN(u16),
+}
+
+impl From<OptionCode> for u16 {
+    fn from(option_code: OptionCode) -> u16 {
+        match option_code {
+            OptionCode::NSID => 3,
+            OptionCode::PADDING => 12,
+            OptionCode::UNKNOWN(val) => val,
+        }
+    }
+}
+
+impl From<u16> for OptionCode {
+    fn from(val: u16) -> OptionCode {
+        match val {
+            3 => OptionCode::NSID,
+            12 => OptionCode::PADDING,
+            _ => OptionCode::UNKNOWN(val),
+        }
+    }
+}
+
+impl From<&str> for OptionCode {
+    fn from(val: &str) -> OptionCode {
+        match val {
+            "NSID" => OptionCode::NSID,
+            "PADDING" => OptionCode::PADDING,
+            _ => OptionCode::UNKNOWN(0),
+        }
+    }
+}
+
+impl Default for OptionCode {
+    fn default() -> Self {
+        OptionCode::UNKNOWN(0)
+    }
+}
+
+impl fmt::Display for OptionCode {
+    fn fmt(&self, f: &mut fmt::Formatter) -> fmt::Result {
+        write!(f, "{}", match *self {
+            OptionCode::NSID => "NSID",
+            OptionCode::PADDING => "PADDING",
+            OptionCode::UNKNOWN(_) => "UNKNOWN",
+        })
+    }
+}

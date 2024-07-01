abdcc7388eeeecf9ddf1934dfcbf4f291d23c9ee
Litr0 <gonzalo.sobarzoa@gmail.com>
2024-07-01 16:25:51 -0400

    refactor: Add support for EDNS0 options in query_a_type_edns test

--- tests/integration_test.rs
+++ tests/integration_test.rs
@@ -132,7 +132,7 @@ async fn no_resource_available() {
 
 #[tokio::test]
 async fn query_a_type_edns() {
-    let response = query_response_edns("example.com", "UDP", "A", Some(1024), 0, 0, None).await;
+    let response = query_response_edns("example.com", "UDP", "A", Some(1024), 0, 0, Some(vec![3])).await;
 
     if let Ok(rrs) = response {
         assert_eq!(rrs.get_answer().len(), 1);
@@ -146,6 +146,7 @@ async fn query_a_type_edns() {
         assert_eq!(opt.get_name(), DomainName::new_from_str(""));
         assert_eq!(opt.get_rtype(), Rrtype::OPT);
         assert_eq!(opt.get_rclass(), Rclass::UNKNOWN(512));
+        println!("{:?}", opt);
     } 
 }
 

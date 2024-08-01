11545ac65d58ed2b273f38d6bd098f09eda4b22b
Katia Fredes <katia.fredes@ug.uchile.cl>
2024-08-01 17:26:03 -0400

    ignore max_number_of_retry test

--- src/async_resolver.rs
+++ src/async_resolver.rs
@@ -951,6 +951,7 @@ mod async_resolver_test {
         // TODO: Test special cases from RFC
     }
 
+    #[ignore = "Taking too long"]
     #[tokio::test]
     async fn max_number_of_retry() {
         let mut config = ResolverConfig::default();

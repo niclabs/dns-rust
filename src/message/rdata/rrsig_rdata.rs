949613bf52fe26e0fe4bc914a714a0d456a0c0a9
joalopez1206 <joalopez@ug.uchile.cl>
2024-09-09 13:31:30 -0300

    fix: RRSIGRdata to encode the signature as base64 not string utf8

--- src/message/rdata/rrsig_rdata.rs
+++ src/message/rdata/rrsig_rdata.rs
@@ -1,8 +1,7 @@
 use crate::message::resource_record::{FromBytes, ToBytes};
 use crate::domain_name::DomainName;
 use crate::message::rrtype::Rrtype;
-use base64::engine::general_purpose;
-use base64::Engine as _;
+use base64;
 use std::fmt;
 
 #[derive(Clone, Debug, PartialEq)]
@@ -144,8 +143,9 @@ impl FromBytes<Result<Self, &'static str>> for RRSIGRdata {
             signature.push(bytes[i]);
             i += 1;
         }
-        //let signature_b64 = general_purpose::STANDARD.encode(signature);
-        rrsig_rdata.set_signature(signature);
+        let signature_b64 = base64::encode(signature);
+        //let signature = String::from_utf8(signature).unwrap();
+        rrsig_rdata.set_signature(signature_b64);
 
         Ok(rrsig_rdata)
         }

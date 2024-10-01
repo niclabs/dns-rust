f1818597e18b485ab01ee108bb5abcdd45aa6c10
joalopez1206 <joalopez@ug.uchile.cl>
2024-10-01 18:09:37 -0300

    new test

--- tests/edns_test.rs
+++ tests/edns_test.rs
@@ -408,14 +408,41 @@ async fn query_from_root() {
         ip2req = rdata.get_address();
     }
 }use std::{net::IpAddr, str::FromStr};
+use std::time::Duration;
 use dns_rust::{async_resolver::{config::ResolverConfig, AsyncResolver}, client::client_error::ClientError, domain_name::DomainName, message::{rclass::Rclass, rdata::Rdata, resource_record::{ResourceRecord, ToBytes}, rrtype::Rrtype, DnsMessage}};
+use dns_rust::async_resolver::server_info::ServerInfo;
 
-async fn query_response_edns(domain_name: &str, 
-    protocol: &str, 
-    qtype: &str, 
-    max_payload: Option<u16>, 
-    version: u8, 
-    do_bit: bool, 
+async fn query_from_ip_with_edns(domain_name: &str,
+                                 protocol: &str,
+                                 qtype: &str,
+                                 max_payload: Option<u16>,
+                                 version: u8,
+                                 do_bit: bool,
+                                 option: Option<Vec<u16>>,
+                                 ip_addr: IpAddr) -> Result<DnsMessage, ClientError> {
+
+    let mut config = ResolverConfig::default();
+    config.add_edns0(max_payload, version, do_bit, option);
+
+
+    config.set_name_servers(vec![ServerInfo::new_from_addr(ip_addr, Duration::from_secs(3))]);
+    let mut resolver = AsyncResolver::new(config);
+
+    let response = resolver.lookup(
+        domain_name,
+        protocol,
+        qtype,
+        "IN").await;
+
+    response.map(|lookup_response| lookup_response.to_dns_msg())
+}
+
+async fn query_response_edns(domain_name: &str,
+    protocol: &str,
+    qtype: &str,
+    max_payload: Option<u16>,
+    version: u8,
+    do_bit: bool,
     option: Option<Vec<u16>>) -> Result<DnsMessage, ClientError> {
 
     let mut config = ResolverConfig::default();
@@ -478,5 +505,32 @@ async fn query_a_type_with_rrsig_edns() {
         assert_eq!(opt.get_rtype(), Rrtype::OPT);
         assert_eq!(opt.get_rclass(), Rclass::UNKNOWN(512));
         println!("{:?}", opt);
-    } 
+    }
+}
+
+#[tokio::test]
+async fn query_from_root() {
+    const ROOTSV1: [u8; 4] = [192,58,128,30];
+
+    let mut ip2req = ROOTSV1.into();
+    let response = query_from_ip_with_edns("example.com",
+                                           "UDP", "A", Some(1024), 0, true,
+                                           Some(vec![3]), ip2req).await;
+    let response = match response {
+        Ok(rrs) => rrs,
+        Err(e) => panic!("{:?}", e),
+    };
+
+    println!("{}", response);
+
+    let additional_rrs = response.get_additional();
+
+    let a_rrs: Vec<_> = additional_rrs.iter()
+        .filter(|arrs|
+            if let Rdata::A(_) = arrs.get_rdata() {true}
+            else {false}).collect();
+
+    if let Rdata::A(rdata) = a_rrs[4].get_rdata() {
+        ip2req = rdata.get_address();
+    }
 }

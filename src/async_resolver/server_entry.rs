d7fb9c1120fe6b5c23892e7213b289b9e8ef8229
Katia <katia.fredes.a@gmail.com>
2024-12-01 22:43:00 +0900

    docs: server entry add documentation

--- src/async_resolver/server_entry.rs
+++ src/async_resolver/server_entry.rs
@@ -3,11 +3,16 @@ use crate::async_resolver::server_info::ServerInfo;
 /// Struct that holds the state of a single server for a request.
 #[derive(Clone)]
 pub struct ServerEntry {
+    /// Information about the server.
     info: ServerInfo,
+    /// The work counter to limit the amount of work done on a single server.
+    /// 
+    /// If the counter reaches zero, the resolver must return a response to the client.
     work_counter: u16,
 }
 
 impl ServerEntry {
+    /// Creates a new ServerEntry for a request.
     pub fn new(info: ServerInfo, work_counter: u16) -> ServerEntry {
         ServerEntry {
             info,
@@ -15,14 +20,17 @@ impl ServerEntry {
         }
     }
 
+    /// Returns a reference to the ServerInfo of the server.
     pub fn get_info(&self) -> &ServerInfo {
         &self.info // TODO: see if this is necessary to use clone or not, in order to reuse TCP connections
     }
 
+    /// Returns the work counter of the server.
     pub fn get_work_counter(&self) -> u16 {
         self.work_counter
     }
 
+    /// Decrements the work counter of the server.
     pub fn decrement_work_counter(&mut self) {
         self.work_counter -= 1;
     }

d271fd0d32f34464bee1d2e94ef8ccea15e996e5
Katia <katia.fredes.a@gmail.com>
2024-12-02 22:24:39 +0900

    change: ServerInfo is wrapped in Arc on Entry

--- src/async_resolver/server_entry.rs
+++ src/async_resolver/server_entry.rs
@@ -1,10 +1,12 @@
+use std::sync::Arc;
+
 use crate::async_resolver::server_info::ServerInfo;
 
 /// Struct that holds the state of a single server for a request.
 #[derive(Clone)]
 pub struct ServerEntry {
     /// Information about the server.
-    info: ServerInfo,
+    info: Arc<ServerInfo>,
     /// The work counter to limit the amount of work done on a single server.
     /// 
     /// If the counter reaches zero, the resolver must return a response to the client.
@@ -13,16 +15,22 @@ pub struct ServerEntry {
 
 impl ServerEntry {
     /// Creates a new ServerEntry for a request.
-    pub fn new(info: ServerInfo, work_counter: u16) -> ServerEntry {
+    /// 
+    /// A reference to a previously created ServerInfo is required to create a ServerEntry. 
+    /// The work counter is used to limit the amount of work done on a single server.
+    pub fn new(info: Arc<ServerInfo>, work_counter: u16) -> ServerEntry {
         ServerEntry {
-            info,
+            info: info,
             work_counter: work_counter,
         }
     }
 
-    /// Returns a reference to the ServerInfo of the server.
-    pub fn get_info(&self) -> &ServerInfo {
-        &self.info // TODO: see if this is necessary to use clone or not, in order to reuse TCP connections
+    /// Returns an atomic reference to the ServerInfo of the server.
+    /// 
+    /// A new  instance of Arc<ServerInfo> is returned, referencing the same ServerInfo 
+    /// as the ServerEntry.
+    pub fn get_info(&self) -> Arc<ServerInfo> {
+        self.info.clone()
     }
 
     /// Returns the work counter of the server.
@@ -34,4 +42,32 @@ impl ServerEntry {
     pub fn decrement_work_counter(&mut self) {
         self.work_counter -= 1;
     }
+}
+
+#[cfg(test)]
+mod tests {
+    use std::net::{IpAddr, Ipv4Addr};
+    use tokio::time::Duration;
+    use crate::client::{client_connection::ClientConnection, tcp_connection::ClientTCPConnection, udp_connection::ClientUDPConnection};
+
+    use super::*;
+
+    #[test]
+    fn constructor() {
+        let ip_addr = IpAddr::V4(Ipv4Addr::new(192, 168, 0, 1));
+        let port = 53;
+        let key = String::from("key");
+        let algorithm = String::from("algorithm");
+        let udp_connection = ClientUDPConnection::new_default(ip_addr, Duration::from_secs(100));
+        let tcp_connection = ClientTCPConnection::new_default(ip_addr, Duration::from_secs(100));
+        let info = ServerInfo::new(ip_addr, port, key, algorithm, udp_connection, tcp_connection);
+
+        let info_arc = Arc::new(info);
+        let work_counter = 2;
+        let server_entry = ServerEntry::new(info_arc, work_counter);
+
+        assert_eq!(server_entry.get_work_counter(), 2);
+
+    }
+
 }

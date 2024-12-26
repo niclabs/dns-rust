2d93374d3daa4c5648bd6d37d20127f27049fb81
jotape24 <jose-pablo1@hotmail.com>
2024-12-26 17:18:28 -0300

    test qtype=MX was added

--- tests/client_test.rs
+++ tests/client_test.rs
@@ -4,7 +4,7 @@ use dns_rust::{
             config::ResolverConfig, resolver_error::ResolverError, AsyncResolver, server_info::ServerInfo
         }, client::{
         client_connection::ClientConnection, client_error::ClientError, tcp_connection::ClientTCPConnection, udp_connection::ClientUDPConnection, Client}, domain_name::DomainName, 
-        message::{resource_record::ResourceRecord, rdata::{a_rdata::ARdata, Rdata}}};
+        message::{resource_record::ResourceRecord, rdata::{a_rdata::ARdata, Rdata, mx_rdata::MxRdata}}};
 
 
 
@@ -35,6 +35,7 @@ async fn QTYPE_A_TEST_AUTH_ANSWER() {
         assert_eq!(RR.get_name(), DomainName::new_from_string("example.com".to_string()));
         assert_eq!(RR.get_rtype(), "A".into());
         assert_eq!(RR.get_rclass(), "IN".into());
+        assert_eq!(RR.get_ttl(), 3600);
         assert_eq!(RR.get_rdlength(), 4);
         assert_eq!(RR.get_rdata(), Rdata::A(ARdata::new_from_addr(IpAddr::V4(Ipv4Addr::new(93, 184, 215, 14)))));
 
@@ -89,11 +90,50 @@ async fn QTYPE_A_TEST() {
 
 
 // RFC 1034 6.2.2
+// Testing with QTYPE=ANY
 
 
 // RFC 1034 6.2.3 
 // Testing with QTYPE=MX
+#[tokio::test]
+async fn QTYPE_MX() {
+    let addr = IpAddr::V4(Ipv4Addr::new(199, 43, 135, 53)); // a.iana-servers.net ip
+    let conn = ClientUDPConnection::new_default(addr, Duration::from_secs(10));
+    let mut client = Client::new(conn);
 
+    let response = client.query(
+        DomainName::new_from_string("example.com".to_string()), 
+        "MX", 
+        "IN"
+    ).await;
+
+
+    if let Ok(resp) = response {
+        // header
+        assert!(resp.get_header().get_qr());
+        assert!(resp.get_header().get_aa());
+
+        // question
+        assert_eq!(resp.get_question(), client.get_dns_query().get_question());
+
+        // answer
+        let RR = &resp.get_answer()[0];
+        assert_eq!(RR.get_name(), DomainName::new_from_string("example.com".to_string()));
+        assert_eq!(RR.get_rtype(), "MX".into());
+        assert_eq!(RR.get_rclass(), "IN".into());
+        assert_eq!(RR.get_ttl(), 86400);
+        assert_eq!(RR.get_rdlength(), 3);
+        assert_eq!(RR.get_rdata(), Rdata::MX(MxRdata::new()));
+
+        // authority
+        assert!(resp.get_authority().is_empty());
+
+        // additional
+        assert!(resp.get_additional().is_empty());
+    } else {
+        panic!("response error");
+    }
+}
 
 // RFC 1034 6.2.4 
 // Testing with QTYPE=NS 

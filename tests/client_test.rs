702335b6e46a258320d67d42ef62c2f32d9dcf98
jotape24 <jose-pablo1@hotmail.com>
2024-12-27 09:08:34 -0300

    test QTYPE=NS partially done

--- tests/client_test.rs
+++ tests/client_test.rs
@@ -1,31 +1,73 @@
-use std::{net::{IpAddr, Ipv4Addr}, str::FromStr, time::Duration};
-use dns_rust::{
-    async_resolver::{
-            config::ResolverConfig, resolver_error::ResolverError, AsyncResolver, server_info::ServerInfo
-        }, client::{
-        client_connection::ClientConnection, client_error::ClientError, tcp_connection::ClientTCPConnection, udp_connection::ClientUDPConnection, Client}, domain_name::DomainName, 
-        message::{resource_record::ResourceRecord, rdata::{a_rdata::ARdata, Rdata, mx_rdata::MxRdata}}};
+#[cfg(test)]
+mod client_test {
+    use std::{net::{IpAddr, Ipv4Addr}, str::FromStr, time::Duration};
+    use dns_rust::{
+        async_resolver::{
+                config::ResolverConfig, resolver_error::ResolverError, AsyncResolver, server_info::ServerInfo
+            }, client::{
+            client_connection::ClientConnection, client_error::ClientError, tcp_connection::ClientTCPConnection, udp_connection::ClientUDPConnection, Client}, domain_name::DomainName, 
+            message::{resource_record::ResourceRecord, rdata::{a_rdata::ARdata, Rdata, mx_rdata::MxRdata, ns_rdata::NsRdata}}};
 
 
 
 
-// RFC 1034 6.2.1 
-// Testing with QTYPE=A on an authoritative server for example.com
-#[tokio::test]
-async fn QTYPE_A_TEST_AUTH_ANSWER() {
-    let addr = IpAddr::V4(Ipv4Addr::new(199, 43, 135, 53)); // a.iana-servers.net ip
-    let conn = ClientUDPConnection::new_default(addr, Duration::from_secs(10));
+    // RFC 1034 6.2.1 
+    // Testing with QTYPE=A on an authoritative server for example.com
+    #[tokio::test]
+    async fn QTYPE_A_TEST_AUTH_ANSWER() {
+        let addr = IpAddr::V4(Ipv4Addr::new(199, 43, 135, 53)); // a.iana-servers.net ip
+        let conn = ClientUDPConnection::new_default(addr, Duration::from_secs(10));
+        let mut client = Client::new(conn);
+
+        let response = client.query(
+            DomainName::new_from_string("example.com".to_string()), 
+            "A", 
+            "IN"
+        ).await;
+        if let Ok(resp) = response {
+            // header
+            assert!(resp.get_header().get_qr());
+            assert!(resp.get_header().get_aa());
+
+            // question
+            assert_eq!(resp.get_question(), client.get_dns_query().get_question());
+
+            // answer
+            let RR = &resp.get_answer()[0];
+            assert_eq!(RR.get_name(), DomainName::new_from_string("example.com".to_string()));
+            assert_eq!(RR.get_rtype(), "A".into());
+            assert_eq!(RR.get_rclass(), "IN".into());
+            assert_eq!(RR.get_ttl(), 3600);
+            assert_eq!(RR.get_rdlength(), 4);
+            assert_eq!(RR.get_rdata(), Rdata::A(ARdata::new_from_addr(IpAddr::V4(Ipv4Addr::new(93, 184, 215, 14)))));
+
+            // authority
+            assert!(resp.get_authority().is_empty());
+
+            // additional
+            assert!(resp.get_additional().is_empty());
+        } else {
+            panic!("response error");
+        }
+    }
+
+
+    // Testing with QTYPE=A on a non authoritative server for example.com
+    #[tokio::test]
+    async fn QTYPE_A_TEST() {
+        let addr = IpAddr::V4(Ipv4Addr::new(1, 1, 1, 1));
+        let conn = ClientUDPConnection::new_default(addr, Duration::from_secs(10));
     let mut client = Client::new(conn);
 
-    let response = client.query(
-        DomainName::new_from_string("example.com".to_string()), 
-        "A", 
-        "IN"
-    ).await;
-    if let Ok(resp) = response {
+        let response = client.query(
+            DomainName::new_from_string("example.com".to_string()), 
+            "A", 
+            "IN"
+        ).await;
+        if let Ok(resp) = response {
         // header
         assert!(resp.get_header().get_qr());
-        assert!(resp.get_header().get_aa());
+        assert!(!resp.get_header().get_aa());
 
         // question
         assert_eq!(resp.get_question(), client.get_dns_query().get_question());
@@ -35,7 +77,6 @@ async fn QTYPE_A_TEST_AUTH_ANSWER() {
         assert_eq!(RR.get_name(), DomainName::new_from_string("example.com".to_string()));
         assert_eq!(RR.get_rtype(), "A".into());
         assert_eq!(RR.get_rclass(), "IN".into());
-        assert_eq!(RR.get_ttl(), 3600);
         assert_eq!(RR.get_rdlength(), 4);
         assert_eq!(RR.get_rdata(), Rdata::A(ARdata::new_from_addr(IpAddr::V4(Ipv4Addr::new(93, 184, 215, 14)))));
 
@@ -44,106 +85,113 @@ async fn QTYPE_A_TEST_AUTH_ANSWER() {
 
         // additional
         assert!(resp.get_additional().is_empty());
-    } else {
-        panic!("response error");
+        } else {
+            panic!();
+        }
     }
-}
 
 
-// Testing with QTYPE=A on a non authoritative server for example.com
-#[tokio::test]
-async fn QTYPE_A_TEST() {
-    let addr = IpAddr::V4(Ipv4Addr::new(1, 1, 1, 1));
-    let conn = ClientUDPConnection::new_default(addr, Duration::from_secs(10));
-   let mut client = Client::new(conn);
+    // RFC 1034 6.2.2
+    // Testing with QTYPE=ANY
 
-    let response = client.query(
-        DomainName::new_from_string("example.com".to_string()), 
-        "A", 
-        "IN"
-    ).await;
-    if let Ok(resp) = response {
-       // header
-       assert!(resp.get_header().get_qr());
-       assert!(!resp.get_header().get_aa());
 
-       // question
-       assert_eq!(resp.get_question(), client.get_dns_query().get_question());
+    // RFC 1034 6.2.3 
+    // Testing with QTYPE=MX
+    #[tokio::test]
+    async fn QTYPE_MX() {
+        let addr = IpAddr::V4(Ipv4Addr::new(199, 43, 135, 53)); // a.iana-servers.net ip
+        let conn = ClientUDPConnection::new_default(addr, Duration::from_secs(10));
+        let mut client = Client::new(conn);
 
-       // answer
-       let RR = &resp.get_answer()[0];
-       assert_eq!(RR.get_name(), DomainName::new_from_string("example.com".to_string()));
-       assert_eq!(RR.get_rtype(), "A".into());
-       assert_eq!(RR.get_rclass(), "IN".into());
-       assert_eq!(RR.get_rdlength(), 4);
-       assert_eq!(RR.get_rdata(), Rdata::A(ARdata::new_from_addr(IpAddr::V4(Ipv4Addr::new(93, 184, 215, 14)))));
+        let response = client.query(
+            DomainName::new_from_string("example.com".to_string()), 
+            "MX", 
+            "IN"
+        ).await;
 
-       // authority
-       assert!(resp.get_authority().is_empty());
 
-       // additional
-       assert!(resp.get_additional().is_empty());
-    } else {
-        panic!();
+        if let Ok(resp) = response {
+            // header
+            assert!(resp.get_header().get_qr());
+            assert!(resp.get_header().get_aa());
+
+            // question
+            assert_eq!(resp.get_question(), client.get_dns_query().get_question());
+
+            // answer
+            let RR = &resp.get_answer()[0];
+            assert_eq!(RR.get_name(), DomainName::new_from_string("example.com".to_string()));
+            assert_eq!(RR.get_rtype(), "MX".into());
+            assert_eq!(RR.get_rclass(), "IN".into());
+            assert_eq!(RR.get_ttl(), 86400);
+            assert_eq!(RR.get_rdlength(), 3);
+            assert_eq!(RR.get_rdata(), Rdata::MX(MxRdata::new()));
+
+            // authority
+            assert!(resp.get_authority().is_empty());
+
+            // additional
+            assert!(resp.get_additional().is_empty());
+        } else {
+            panic!("response error");
+        }
     }
-}
+
+    // RFC 1034 6.2.4 
+    // Testing with QTYPE=NS 
+    #[tokio::test]
+    async fn QTYPE_NS() {
+        let addr = IpAddr::V4(Ipv4Addr::new(199, 43, 135, 53)); // a.iana-servers.net ip
+        let conn = ClientUDPConnection::new_default(addr, Duration::from_secs(10));
+        let mut client = Client::new(conn);
+
+        let response = client.query(
+            DomainName::new_from_string("example.com".to_string()), 
+            "NS", 
+            "IN"
+        ).await;
 
 
-// RFC 1034 6.2.2
-// Testing with QTYPE=ANY
+        if let Ok(resp) = response {
+            // header
+            assert!(resp.get_header().get_qr());
+            assert!(resp.get_header().get_aa());
 
+            // question
+            assert_eq!(resp.get_question(), client.get_dns_query().get_question());
 
-// RFC 1034 6.2.3 
-// Testing with QTYPE=MX
-#[tokio::test]
-async fn QTYPE_MX() {
-    let addr = IpAddr::V4(Ipv4Addr::new(199, 43, 135, 53)); // a.iana-servers.net ip
-    let conn = ClientUDPConnection::new_default(addr, Duration::from_secs(10));
-    let mut client = Client::new(conn);
+            // answer
+            let RR = &resp.get_answer()[0];
+            assert_eq!(RR.get_name(), DomainName::new_from_string("example.com".to_string()));
+            assert_eq!(RR.get_rtype(), "NS".into());
+            assert_eq!(RR.get_rclass(), "IN".into());
+            assert_eq!(RR.get_ttl(), 86400);
+            assert_eq!(RR.get_rdlength(), 3);
 
-    let response = client.query(
-        DomainName::new_from_string("example.com".to_string()), 
-        "MX", 
-        "IN"
-    ).await;
+            //TODO
+            //data = Rdata::NS(NsRdata::rr_from_master_file(
 
+            //));
+            //assert_eq!(RR.get_rdata(), data);
 
-    if let Ok(resp) = response {
-        // header
-        assert!(resp.get_header().get_qr());
-        assert!(resp.get_header().get_aa());
+            // authority
+            assert!(resp.get_authority().is_empty());
 
-        // question
-        assert_eq!(resp.get_question(), client.get_dns_query().get_question());
-
-        // answer
-        let RR = &resp.get_answer()[0];
-        assert_eq!(RR.get_name(), DomainName::new_from_string("example.com".to_string()));
-        assert_eq!(RR.get_rtype(), "MX".into());
-        assert_eq!(RR.get_rclass(), "IN".into());
-        assert_eq!(RR.get_ttl(), 86400);
-        assert_eq!(RR.get_rdlength(), 3);
-        assert_eq!(RR.get_rdata(), Rdata::MX(MxRdata::new()));
-
-        // authority
-        assert!(resp.get_authority().is_empty());
-
-        // additional
-        assert!(resp.get_additional().is_empty());
-    } else {
-        panic!("response error");
+            // additional
+            assert!(resp.get_additional().is_empty());
+        } else {
+            panic!("response error");
+        }
     }
+
+
+    // RFC 1034 6.2.5 
+
+    // RFC 1034 6.2.6 
+
+    // RFC 1034 6.2.7 
+
+    // RFC 1034 6.2.8
+
+
 }
-
-// RFC 1034 6.2.4 
-// Testing with QTYPE=NS 
-
-// RFC 1034 6.2.5 
-
-// RFC 1034 6.2.6 
-
-// RFC 1034 6.2.7 
-
-// RFC 1034 6.2.8
-
-

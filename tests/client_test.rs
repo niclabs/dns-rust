949cd196e8c6e2270a360a08493979d18d5e05da
jotape24 <jose-pablo1@hotmail.com>
2024-12-27 09:48:01 -0300

    test QTYPE=ANY with auth answer

--- tests/client_test.rs
+++ tests/client_test.rs
@@ -92,7 +92,46 @@ mod client_test {
 
 
     // RFC 1034 6.2.2
-    // Testing with QTYPE=ANY
+    // Testing with QTYPE=ANY on an authoritative server for example.com
+    #[tokio::test]
+    async fn QTYPE_ANY_AUTH_ANSWER() {
+        let addr = IpAddr::V4(Ipv4Addr::new(199, 43, 135, 53)); // a.iana-servers.net ip
+        let conn = ClientUDPConnection::new_default(addr, Duration::from_secs(10));
+        let mut client = Client::new(conn);
+
+        let response = client.query(
+            DomainName::new_from_string("example.com".to_string()), 
+            "ANY", 
+            "IN"
+        ).await;
+
+
+        if let Ok(resp) = response {
+            // header
+            assert!(resp.get_header().get_qr());
+            assert!(resp.get_header().get_aa());
+
+            // question
+            assert_eq!(resp.get_question(), client.get_dns_query().get_question());
+
+            // answer
+            let RR = &resp.get_answer()[0];
+            assert_eq!(RR.get_name(), DomainName::new_from_string("example.com".to_string()));
+            assert_eq!(RR.get_rtype(), "A".into());
+            assert_eq!(RR.get_rclass(), "IN".into());
+            assert_eq!(RR.get_ttl(), 3600);
+            assert_eq!(RR.get_rdlength(), 4);
+            assert_eq!(RR.get_rdata(), Rdata::A(ARdata::new_from_addr(IpAddr::V4(Ipv4Addr::new(93, 184, 215, 14)))));
+
+            // authority
+            assert!(resp.get_authority().is_empty());
+
+            // additional
+            assert!(resp.get_additional().is_empty());
+        } else {
+            panic!("response error");
+        }
+    }
 
 
     // RFC 1034 6.2.3 
@@ -166,9 +205,11 @@ mod client_test {
             assert_eq!(RR.get_rtype(), "NS".into());
             assert_eq!(RR.get_rclass(), "IN".into());
             assert_eq!(RR.get_ttl(), 86400);
-            assert_eq!(RR.get_rdlength(), 3);
 
-            //TODO
+            // FIX
+            assert_eq!(RR.get_rdlength(), 3);
+            
+            // TODO
             //data = Rdata::NS(NsRdata::rr_from_master_file(
 
             //));

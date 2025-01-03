b1d6a7136b1b41b0dac00d84d07f2ccd2ac817e1
jotape24 <jose-pablo1@hotmail.com>
2025-01-03 18:10:54 -0300

    refactor option_code moved to opt_option

--- src/edns/opt_option.rs
+++ src/edns/opt_option.rs
@@ -1,4 +1,4 @@
-use crate::message::rdata::opt_rdata::option_code::OptionCode;
+use crate::edns::opt_option::option_code::OptionCode;
 use crate::edns::opt_option::option_data::OptionData;
 
 #[derive(Debug, Clone, PartialEq, Eq)]
@@ -30,7 +30,7 @@ impl OptOption {
 
 pub mod option_data {
     use crate::edns::options::ede::ede_optdata::EdeOptData;
-    use crate::message::rdata::opt_rdata::option_code::OptionCode;
+    use crate::edns::opt_option::option_code::OptionCode;
     use crate::message::resource_record::ToBytes;
 
     #[derive(Debug, Clone, PartialEq, Eq)]
@@ -69,6 +69,85 @@ pub mod option_data {
                 },
                 _ => Ok(OptionData::Unknown(bytes))
             }
+        }
+    }
+}
+
+pub mod option_code {
+    use std::fmt;
+    #[derive(Clone, PartialEq, Debug, Hash, PartialOrd, Ord, Eq, Copy)]
+    /// Enum for the option code in an OPT Rdata
+    pub enum OptionCode {
+        NSID,
+        PADDING,
+        UNKNOWN(u16),
+        // added for rfc6975
+        DAU,
+        DHU,
+        // added for rf8914
+        EDE,
+        N3U
+    }
+
+    impl From<OptionCode> for u16 {
+        fn from(option_code: OptionCode) -> u16 {
+            match option_code {
+                OptionCode::NSID => 3,
+                OptionCode::DAU => 5,
+                OptionCode::DHU => 6,
+                OptionCode::N3U => 7,
+                OptionCode::PADDING => 12,
+                OptionCode::EDE => 15,
+                OptionCode::UNKNOWN(val) => val,
+            }
+        }
+    }
+
+    impl From<u16> for OptionCode {
+        fn from(val: u16) -> OptionCode {
+            match val {
+                3 => OptionCode::NSID,
+                5 => OptionCode::DAU,
+                6 => OptionCode::DHU,
+                7 => OptionCode::N3U,
+                12 => OptionCode::PADDING,
+                15 => OptionCode::EDE,
+                _ => OptionCode::UNKNOWN(val),
+            }
+        }
+    }
+
+    impl From<&str> for OptionCode {
+        fn from(val: &str) -> OptionCode {
+            match val {
+                "NSID" => OptionCode::NSID,
+                "DAU" => OptionCode::DAU,
+                "DHU" => OptionCode::DHU,
+                "N3U" => OptionCode::N3U,
+                "EDE" => OptionCode::EDE,
+                "PADDING" => OptionCode::PADDING,
+                _ => OptionCode::UNKNOWN(0),
+            }
+        }
+    }
+
+    impl Default for OptionCode {
+        fn default() -> Self {
+            OptionCode::UNKNOWN(0)
+        }
+    }
+
+    impl fmt::Display for OptionCode {
+        fn fmt(&self, f: &mut fmt::Formatter) -> fmt::Result {
+            write!(f, "{}", match *self {
+                OptionCode::NSID => "NSID",
+                OptionCode::DAU => "DAU",
+                OptionCode::DHU => "DHU",
+                OptionCode::N3U => "N3U",
+                OptionCode::EDE => "EDE",
+                OptionCode::PADDING => "PADDING",
+                OptionCode::UNKNOWN(_) => "UNKNOWN",
+            })
         }
     }
 }

d08166ef96192ef8bc8bd40c82eb441385eeeaa3
jotape24 <jose-pablo1@hotmail.com>
2025-01-13 21:53:35 -0300

    test client_edns_two_options added in client_edns.rs

--- tests/client_edns.rs
+++ tests/client_edns.rs
@@ -1,6 +1,5 @@
 use std::net::{IpAddr, Ipv4Addr};
 use std::time::Duration;
-use futures_util::TryFutureExt;
 use dns_rust::client::Client;
 use dns_rust::client::client_connection::ClientConnection;
 use dns_rust::client::udp_connection::ClientUDPConnection;
@@ -23,7 +22,7 @@ async fn client_edns_ede_code() {
     let conn = ClientUDPConnection::new_default(addr, Duration::from_secs(10));
     let mut client = Client::new(conn);
 
-    // messsage
+    // message
     let mut dns_query_message =
         DnsMessage::new_query_message(
             DomainName::new_from_string("nonexistent.com".to_string()),
@@ -63,6 +62,64 @@ async fn client_edns_ede_code() {
                 let mut expected = OptOption::new(OptionCode::EDE);
                 expected.set_opt_data(OptionData::EDE(EdeOptData::new(EdeCode::from(22), "At delegation nonexistent.com for nonexistent.com/a".to_string())));
                 assert_eq!(options[0], expected);
+
+            },
+            _ =>{}
+        }
+    }
+}
+
+
+#[tokio::test]
+async fn client_edns_two_options() {
+    // client
+    let addr = IpAddr::V4(Ipv4Addr::new(8, 8, 8, 8));
+    let conn = ClientUDPConnection::new_default(addr, Duration::from_secs(10));
+    let mut client = Client::new(conn);
+
+    // message
+    let mut dns_query_message =
+        DnsMessage::new_query_message(
+            DomainName::new_from_string("nonexistent.com".to_string()),
+            Rrtype::A,
+            Rclass::IN,
+            0,
+            false,
+            1);
+
+    dns_query_message.add_edns0(None, Rcode::NOERROR, 0, true,Some(vec![OptionCode::EDE, OptionCode::NSID]));
+
+    client.set_dns_query(dns_query_message);
+    let res = client.send_query().await;
+
+    if let Ok(response) = res {
+        let additional = response.get_additional();
+
+        assert_eq!(additional.len(), 1);
+
+        let rr = &additional[0];
+
+        assert_eq!(rr.get_name().get_name(), String::from("."));
+
+        assert_eq!(rr.get_rtype(), Rrtype::OPT);
+
+        assert_eq!(rr.get_rclass(), Rclass::UNKNOWN(512));
+
+        assert_eq!(rr.get_ttl(), 32768);
+
+        assert_eq!(rr.get_rdlength(), 8);
+
+        let rdata = rr.get_rdata();
+
+        match rdata {
+            Rdata::OPT(opt) => {
+                let options = opt.get_option();
+                let mut first_expected = OptOption::new(OptionCode::EDE);
+                first_expected.set_opt_data(OptionData::EDE(EdeOptData::new(EdeCode::from(22), "At delegation nonexistent.com for nonexistent.com/a".to_string())));
+                assert_eq!(options[0], first_expected);
+
+                let mut second_expected = OptOption::new(OptionCode::NSID);
+                second_expected.set_opt_data(OptionData::from_bytes_with_opt_type("iad.aservers.dns.icann.org".to_string().into_bytes(), OptionCode::NSID).unwrap());
 
             },
             _ =>{}
